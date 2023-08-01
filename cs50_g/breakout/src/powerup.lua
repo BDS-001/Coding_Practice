b8c86238d560a6640c942304b09c10622f4a26d9
BDS <113209967+BDS-001@users.noreply.github.com>
2023-07-31 20:49:06 -0400

    started tracking cs50g

---
+++ cs50_g/breakout/src/powerup.lua
@@ -0,0 +1,41 @@
+Powerup = class()
+
+function Powerup:init(powerup)
+    self.width = 16
+    self.height = 16
+
+    self.x = math.random(0, VIRTUAL_WIDTH - 16)
+    self.y = 0
+
+    self.dy = 5
+
+    self.powerup = powerup
+end
+
+function Powerup:collides(target)
+    -- first, check to see if the left edge of either is farther to the right
+    -- than the right edge of the other
+    if self.x > target.x + target.width or target.x > self.x + self.width then
+        return false
+    end
+
+    -- then check to see if the bottom edge of either is higher than the top
+    -- edge of the other
+    if self.y > target.y + target.height or target.y > self.y + self.height then
+        return false
+    end 
+
+    -- if the above aren't true, they're overlapping
+    return true
+end
+
+function Powerup:update(dt)
+    self.y = self.y + self.dy * dt
+end
+
+function Ball:render()
+    -- gTexture is our global texture for all blocks
+    -- gBallFrames is a table of quads mapping to each individual ball skin in the texture
+    love.graphics.draw(gTextures['main'], gFrames['powerups'][self.powerup],
+        self.x, self.y)
+end
