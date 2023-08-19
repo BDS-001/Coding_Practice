01d21194434932d394ec4d0ff59e94174f5b5055
BDS <113209967+BDS-001@users.noreply.github.com>
2023-08-19 14:49:21 -0400

    with javascript: added a red p tag, blue h3tag, pink div with a p and h1 element inside

--- odin_project/jsDOM/DOM-manipulation.js
+++ odin_project/jsDOM/DOM-manipulation.js
@@ -4,3 +4,21 @@ let p = document.createElement('p')
 p.setAttribute('style', 'color: red;')
 p.innerText = 'This is red'
 body.appendChild(p)
+
+
+const h3 = document.createElement('h3')
+h3.setAttribute('style', 'color: blue;')
+h3.innerText = 'im a blue h3!'
+body.appendChild(h3)
+
+let div = document.createElement('div')
+div.setAttribute('style', 'border: 1px solid black; background-color: pink;')
+body.appendChild(div)
+
+const h1 = document.createElement('h1')
+h1.innerText = 'im in a div'
+div.appendChild(h1)
+
+p = document.createElement('p')
+p.innerText = 'me too'
+div.appendChild(p)
