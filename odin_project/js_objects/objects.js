ba92586cd60d9b02cb019d3b824a94f12ffd545b
BDS <113209967+BDS-001@users.noreply.github.com>
2023-10-09 12:18:49 -0400

    practice prototypes

--- odin_project/js_objects/objects.js
+++ odin_project/js_objects/objects.js
@@ -8,5 +8,21 @@ function Book(title, author, pages, read) {
     }
 }
 
+Book.prototype.basic = function() {
+    return `${this.title} by ${this.author}`
+}
+
 const hobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', 'not read yet')
 console.log(hobbit.info());
+console.log(hobbit.basic())
+
+
+function Movie(title, author) {
+    this.title = title
+    this.author = author
+}
+
+Movie.prototype = Book.prototype
+
+const lionKing = new Movie('Lion King', 'Disney')
+console.log(lionKing.basic())
