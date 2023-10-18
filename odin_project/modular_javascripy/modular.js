6203c7ce8a8d0435a66b33311bb6e98acc95873b
BDS <113209967+BDS-001@users.noreply.github.com>
2023-10-17 20:56:38 -0400

    practice modular js

--- odin_project/modular_javascripy/modular.js
+++ odin_project/modular_javascripy/modular.js
@@ -1,27 +1,47 @@
-let myModule = {
-    name: 'will',
-    age: 34,
-    sayName: function() {
-        alert(this.name)
+// let myModule = {
+//     name: 'will',
+//     age: 34,
+//     sayName: function() {
+//         alert(this.name)
+//     },
+//     setName: function(newName) {
+//         this.name = newName
+//     }
+// }
+
+// myModule.sayName()
+
+// //only module can access variables
+// let people = (function() {
+//     let name = 'will'
+
+//     function sayName() {
+//         alert(name);
+//     }
+
+//     return {
+//         sayName:sayName
+//     }
+// })()
+
+let poepleTwo = {
+    people: [],
+    init: function() {
+        this.cacheDom()
     },
-    setName: function(newName) {
-        this.name = newName
+    cacheDom: function() {
+        this.el = document.querySelector('#peopleModule')
+        this.button = this.el.querySelector('button')
+        this.input = this.el.querySelector('input')
+        this.ul = this.el.querySelector('ul')
+        this.template = document.querySelector('#people-template')
+    },
+    render: function() {
+        let data = {
+            people: this.people,
+            
+        }
     }
+    
+
 }
-
-myModule.sayName()
-
-//only module can access variables
-let people = (function() {
-    let name = 'will'
-
-    function sayName() {
-        alert(name);
-    }
-
-    return {
-        sayName:sayName
-    }
-})()
-
-
