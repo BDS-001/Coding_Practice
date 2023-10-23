74b03c5a9d17c98adc436f7169f0e242dda37f8d
BDS <113209967+BDS-001@users.noreply.github.com>
2023-10-23 11:57:54 -0400

    practice classes

---
+++ odin_project/js-classes/classes.js
@@ -0,0 +1,25 @@
+class User {
+
+    constructor(name) {
+      // invokes the setter
+      this.name = name;
+    }
+  
+    get name() {
+      return this._name;
+    }
+  
+    set name(value) {
+      if (value.length < 4) {
+        console.log("Name is too short.");
+        return;
+      }
+      this._name = value;
+    }
+  
+  }
+
+  let test = new User("John");
+  console.log(test.name);
+  
+  test = new User(""); 
