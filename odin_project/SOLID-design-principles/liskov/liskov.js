069a2dbde11105a5c3a01df338f93518ba68e93d
BDS <113209967+BDS-001@users.noreply.github.com>
2023-11-14 21:52:22 -0500

    practice liskov principle

--- odin_project/SOLID-design-principles/liskov/liskov.js
+++ odin_project/SOLID-design-principles/liskov/liskov.js
@@ -27,8 +27,22 @@ class Square extends Rectangle {
         this.width = height
         this.height = height
     }
-
-    area() {
-        return this.width * this.height
-    }
 }
+
+function increaseRectangleWidth(rectangle) {
+    rectangle.setWidth(rectangle.width + 1)
+}
+
+const rec1 = new Rectangle(15, 40)
+const rec2 = new Rectangle(22, 22)
+const sqr1 = new Square(22, 22)
+
+increaseRectangleWidth(rec1)
+increaseRectangleWidth(rec2)
+increaseRectangleWidth(sqr1)
+
+console.log(rec1.area())
+console.log(rec2.area())
+console.log(sqr1.area())
+
+// the principle is broken becuase increaseRectangleWidth function doesnt work correctly with rectangle and square, square increasees both the height and width
