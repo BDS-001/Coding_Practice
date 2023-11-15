ccdff723340f25bb82f0e1b18d26a45629865be1
BDS <113209967+BDS-001@users.noreply.github.com>
2023-11-14 21:56:43 -0500

    practice liskov principle

---
+++ odin_project/SOLID-design-principles/liskov/fixedLiskov.js
@@ -0,0 +1,50 @@
+//fix by creating a new class to inherit from
+
+class Shape {
+    constructor(width, height) {
+        this.width = width
+        this.height = height
+    }
+
+    area() {
+        return this.width * this.height
+    }
+}
+
+class Rectangle extends Shape{
+    setWidth(width) {
+        this.width = width
+    }
+
+    setHeight(height) {
+        this.height = height
+    }
+}
+
+class Square extends Shape {
+    setWidth(width) {
+        this.width = width
+        this.height = width
+    }
+
+    setHeight(height) {
+        this.width = height
+        this.height = height
+    }
+}
+
+function increaseShapeWidth(rectangle) {
+    rectangle.setWidth(rectangle.width + 1)
+}
+
+const rec1 = new Rectangle(15, 40)
+const rec2 = new Rectangle(22, 22)
+const sqr1 = new Square(22, 22)
+
+increaseRectangleWidth(rec1)
+increaseRectangleWidth(rec2)
+increaseRectangleWidth(sqr1)
+
+console.log(rec1.area())
+console.log(rec2.area())
+console.log(sqr1.area())
