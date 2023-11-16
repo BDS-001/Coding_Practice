2bfe33303e1e68513769ff5117e70bc663a8f08c
BDS <113209967+BDS-001@users.noreply.github.com>
2023-11-15 19:25:48 -0500

    practice interface segregation principle

--- odin_project/SOLID-design-principles/interface-segregation/fixed-interface-segregation.js
+++ odin_project/SOLID-design-principles/interface-segregation/fixed-interface-segregation.js
@@ -1,19 +1,23 @@
 class Entity {
-    constructor(name, attackDamage, health) {
+    constructor(name) {
         this.name = name
-        this.attackDamage = attackDamage
-        this.health = health
     }
+}
 
+const mover = {
     move() {
         console.log(`${this.name} moved`)
     }
+}
 
+const attacker = {
     attack(target) {
         console.log(`${this.name} attacked ${target.name} for ${this.attackDamage} damage`)
         target.takeDamage(this.attackDamage)
     }
+}
 
+const takeDamage = {
     takeDamage(amount) {
         this.health -= amount
         console.log(`${this.health} health remaining`)
@@ -21,33 +25,31 @@ class Entity {
 }
 
 class Character extends Entity {
-
+    constructor(name, attackDamage, health) {
+        super(name)
+        this.attackDamage = attackDamage
+        this.health = health
+    }
 }
+
+Object.assign(Character.prototype, mover)
+Object.assign(Character.prototype, attacker)
+Object.assign(Character.prototype, takeDamage)
 
 class Wall extends Entity {
     constructor(name, health) {
-        super(name, 0, health)
-    }
-
-    move() {
-        return null
-    }
-
-    attack() {
-        return null
+        super(name)
+        this.health = health
     }
 }
+
+Object.assign(Wall.prototype, takeDamage)
 
 class Turret extends Entity {
     constructor(name, attackDamage) {
-        super(name, attackDamage, -1)
-    }
-
-    move() {
-        return null
-    }
-
-    takeDamage() {
-        return null
+        super(name)
+        this.attackDamage = attackDamage
     }
 }
+
+Object.assign(Turret.prototype, attacker)
