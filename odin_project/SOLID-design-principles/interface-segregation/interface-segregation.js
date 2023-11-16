8ba8ccc01b2eb5d17c31f939ae7ece7523cd850e
BDS <113209967+BDS-001@users.noreply.github.com>
2023-11-15 19:17:42 -0500

    practice interface segregation principle

--- odin_project/SOLID-design-principles/interface-segregation/interface-segregation.js
+++ odin_project/SOLID-design-principles/interface-segregation/interface-segregation.js
@@ -19,3 +19,35 @@ class Entity {
         console.log(`${this.health} health remaining`)
     }
 }
+
+class Character extends Entity {
+
+}
+
+class Wall extends Entity {
+    constructor(name, health) {
+        super(name, 0, health)
+    }
+
+    move() {
+        return null
+    }
+
+    attack() {
+        return null
+    }
+}
+
+class Turret extends Entity {
+    constructor(name, attackDamage) {
+        super(name, attackDamage, -1)
+    }
+
+    move() {
+        return null
+    }
+
+    attack() {
+        return null
+    }
+}
