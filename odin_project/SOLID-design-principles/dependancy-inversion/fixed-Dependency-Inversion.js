6b3b7e8fd4d6cdc2e582688ddf999f0b0c4d2015
BDS <113209967+BDS-001@users.noreply.github.com>
2023-11-16 22:09:38 -0500

    practice dependancy inversion

---
+++ odin_project/SOLID-design-principles/dependancy-inversion/fixed-Dependency-Inversion.js
@@ -0,0 +1,53 @@
+class Store {
+    constructor(paymentProcessor) {
+        this.paymentProcessor = paymentProcessor
+    }
+
+    purchaseBike(quantity) {
+        this.paymentProcessor.pay(200 * quantity)
+    }
+
+    purchasePokeBall(quantity) {
+        this.paymentProcessor.pay(20 * quantity)
+    }
+}
+
+//stripe api
+class Stripe {
+    constructor(user) {
+        this.user = user
+    }
+
+    makePayment(amountInCents) {
+        console.log(`${this.user} made a payment of $${amountInCents / 100} with stripe`)
+    }
+}
+
+//paypal api
+class Paypal {
+    makePayment(user, amountInDollars) {
+        console.log(`${user} made a payment of $${amountInDollars} with paypal`)
+    }
+}
+
+//api wrapper that has the same function for all other payment wrappers
+class StripePaymentProcessor {
+    constructor(user) {
+        this.stripe = new Stripe(user)
+    }
+
+    pay(amountInDollars) {
+        this.stripe.makePayment(amountInDollars * 100)
+    } 
+}
+
+//api wrapper that has the same function for all other payment wrappers
+class PaypalPaymentProcessor {
+    constructor(user) {
+        this.paypal = new Paypal(user)
+    }
+
+    pay(amountInDollars) {
+        this.paypal.makePayment(amountInDollars)
+    } 
+}
