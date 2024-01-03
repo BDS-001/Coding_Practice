86e8051c260822ded78f2ef3d145b6d383ca0440
BDS <113209967+BDS-001@users.noreply.github.com>
2024-01-02 20:23:42 -0500

    added image slider

--- odin_project/dynamic_user_interface/scripts.js
+++ odin_project/dynamic_user_interface/scripts.js
@@ -14,3 +14,47 @@ function showMenu() {
 }
 
 dropdownButton.addEventListener('click', showMenu)
+
+const leftArrow = document.getElementById('left-arrow')
+const rightArrow = document.getElementById('right-arrow')
+const display = document.getElementById('display')
+
+const images = ['test1', 'test2', 'test3']
+const imagePath = './static/images/'
+let pointer = 0
+
+let intervalId
+const intervalDuration = 5000
+
+function slide(interval) {
+    if (pointer + interval < 0) {
+        pointer = images.length - 1
+    } else if (pointer + interval >= images.length) {
+        pointer = 0
+    } else {
+        pointer = pointer + interval
+    }
+    
+    display.src = `${imagePath}${images[pointer]}.png` 
+}
+
+function startSlideInterval() {
+    // Clear any existing interval
+    clearInterval(intervalId);
+
+    // Set a new interval
+    intervalId = setInterval(() => slide(1), intervalDuration);
+}
+
+leftArrow.addEventListener('click', () => {
+    slide(-1);
+    startSlideInterval()
+});
+rightArrow.addEventListener('click', () => {
+    slide(1);
+    startSlideInterval()
+});
+
+const pageSetup = (function() {
+    startSlideInterval()
+})();
