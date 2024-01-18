5c2659dca45853e078eaccd04710ae63cf199e69
BDS <113209967+BDS-001@users.noreply.github.com>
2024-01-17 20:10:01 -0500

    api practice

---
+++ odin_project/api/giphy/scripts.js
@@ -0,0 +1,17 @@
+const inputBox = document.getElementById('api-key');
+const searchButton = document.getElementById('search');
+const img = document.querySelector('img')
+
+searchButton.addEventListener('click', function() {
+    const apiKey = inputBox.value;
+    searchGif
+});
+function searchGif(apiKey, search) {
+    fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${apiKey}&s=${search}`, {mode: 'cors'})
+    .then(function(response) {
+    return response.json();
+    })
+    .then(function(response) {
+    img.src = response.data.images.original.url;
+    });
+}
