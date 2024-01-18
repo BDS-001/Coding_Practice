755eae5d152a2c4823bd9e78234ae1a8c8622d2f
BDS <113209967+BDS-001@users.noreply.github.com>
2024-01-17 20:10:58 -0500

    api practice

--- odin_project/api/giphy/scripts.js
+++ odin_project/api/giphy/scripts.js
@@ -4,8 +4,9 @@ const img = document.querySelector('img')
 
 searchButton.addEventListener('click', function() {
     const apiKey = inputBox.value;
-    searchGif
+    searchGif(apiKey, 'cats')
 });
+
 function searchGif(apiKey, search) {
     fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${apiKey}&s=${search}`, {mode: 'cors'})
     .then(function(response) {
