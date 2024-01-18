c47ac26f9d55f1680e53e3f33dce2cfdac9f5845
BDS <113209967+BDS-001@users.noreply.github.com>
2024-01-17 20:33:19 -0500

    added error handling

--- odin_project/api/giphy/scripts.js
+++ odin_project/api/giphy/scripts.js
@@ -1,18 +1,31 @@
 const inputBox = document.getElementById('api-key');
 const searchButton = document.getElementById('search');
+const searchBox = document.getElementById('search-box');
 const img = document.querySelector('img')
 
 searchButton.addEventListener('click', function() {
     const apiKey = inputBox.value;
-    searchGif(apiKey, 'cats')
+    const search = searchBox.value;
+
+    if (!search) {
+        search = 'cats'
+    }
+    searchGif(apiKey, search)
 });
 
 function searchGif(apiKey, search) {
     fetch(`https://api.giphy.com/v1/gifs/translate?api_key=${apiKey}&s=${search}`, {mode: 'cors'})
     .then(function(response) {
+    if (!response.ok) {
+        throw new Error('Error, status = ' + response.status);
+    }
     return response.json();
     })
     .then(function(response) {
+        console.log(response)
     img.src = response.data.images.original.url;
+    })
+    .catch(function(error) {
+        console.error('Error:', error);
     });
 }
