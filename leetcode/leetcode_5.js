6fd316e543692d659636b51800a8043cd28d0c79
BDS <113209967+BDS-001@users.noreply.github.com>
2024-01-22 23:23:17 -0500

    attempt leetcode #5

---
+++ leetcode/leetcode_5.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+    let p1 = 0
+    let p2 = 1
+
+    let longest = ''
+    function palindrome(str) {
+        return str === str.split('').reverse().join('');
+    }
+
+    while(p1 < s.length) {
+        let sub = s.substring(p1, p2)
+        if (palindrome(sub)) {
+            longest = longest.length < sub.length ? sub : longest;
+        }
+
+        p2++;
+        if (p2 >= s.length) {
+            p1++;
+            p2 = p1 + 1;
+        }
+    }
+    return longest
+};
