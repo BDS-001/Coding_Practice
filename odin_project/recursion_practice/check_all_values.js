c645c056d494574007b6cda527ee4241af19a968
BDS <113209967+BDS-001@users.noreply.github.com>
2024-02-11 20:31:27 -0500

    practice recursion

--- odin_project/recursion_practice/check_all_values.js
+++ odin_project/recursion_practice/check_all_values.js
@@ -2,13 +2,19 @@ function all(arr, func) {
     const curr = arr.pop()
     if (!curr) return true
     if (!func(curr)) return false
-    all(arr, func)
+    return all(arr, func)
 }
 
 
 
 //test
-const allAreLessThanSeven = all([1,2,9], function(num){
+let allAreLessThanSeven = all([1,2,9], function(num){
+	return num < 7;
+});
+
+console.log(allAreLessThanSeven);
+
+allAreLessThanSeven = all([1,2,4], function(num){
 	return num < 7;
 });
 
