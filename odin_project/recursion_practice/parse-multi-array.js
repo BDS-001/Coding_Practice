d0648779f340bec0dd3edc5f7916a75ebda58d1f
BDS <113209967+BDS-001@users.noreply.github.com>
2024-02-14 20:55:11 -0500

    practice recursion

--- odin_project/recursion_practice/parse-multi-array.js
+++ odin_project/recursion_practice/parse-multi-array.js
@@ -1,13 +1,20 @@
 function totalIntegers(array) {
+    if (array.length === 0) return 0;
+
     let ints = 0
-    for (let index = 0; index < array.length; index++) {
-        if (Number.isInteger(array[index])) {
-            ints ++
-        }
+    const current = array.pop()
+    
+    if (Number.isInteger(current)) {
+        ints ++;
+    }else if (Array.isArray(current)) {
+        ints += totalIntegers(current)
     }
-    for (let index = 0; index < array.length; index++) {
-        if (Array.isArray(array[index])) {
-            return
-        }
-    }
+
+    return ints + totalIntegers(array)
 }
+
+
+
+//test
+var seven = totalIntegers([[[5], 3], 0, 2, ['foo'], [], [4, [5, 6]]]); // 7
+console.log(seven);
