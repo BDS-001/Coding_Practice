0aedb80e021de63f71b02545c7724a9e9bf3f124
BDS <113209967+BDS-001@users.noreply.github.com>
2024-02-26 21:50:57 -0500

    created linked lsit factory function with the ability to append/prepend/chck size of linked list

--- odin_project/Project-linked-lists/linked-list.js
+++ odin_project/Project-linked-lists/linked-list.js
@@ -1,6 +1,46 @@
-const node = (function () {
+const node = (function (val=null) {
     return {
-        value: null,
+        value: val,
         nextNode: null
+    }
+})
+
+const linkedList = (function() {
+    let list = null
+    let size = 0
+
+    function append(val) {
+        if (!list) {
+            list = node(val)
+        } else {
+            let pointer = list
+            while (pointer) {
+                pointer.nextNode ? pointer = pointer.nextNode : pointer.nextNode = node(val)
+            }
+        }
+    }
+
+    function prepend(val) {
+        if (!list) {
+            list = node(val)
+        } else {
+            let newNode = node(val)
+            newNode.nextNode = list
+            list = newNode
+        }
+    }
+
+    function size() {
+        let size = 0
+        const pointer = list
+        while (pointer) {
+            size += 1
+            pointer = pointer.nextNode
+        }
+        return size
+    }
+
+    function head() {
+        return list
     }
 })
