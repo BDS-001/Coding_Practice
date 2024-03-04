f1af19933189e4edce46d3c8819024322716e57e
BDS <113209967+BDS-001@users.noreply.github.com>
2024-03-03 22:25:06 -0500

    practiciing leetcode 14

---
+++ leetcode/leetcode-14.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    let longestPref = ''
+    let start = 0
+    let end = 1
+
+    while (true) {
+        const temp = strs[0].slice(start, end)
+
+        if (strs.every(str => str.includes(temp))) {
+            if (temp.length > longestPref) longestPref = temp;
+            end += 1
+        } else {
+            start += 1
+            if (start === end) end += 1
+            if (end === strs[0].length + 1) break
+        }
+    }
+};
