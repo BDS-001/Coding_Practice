e095e04e41594e48702c4170651646eda109edd7
BDS <113209967+BDS-001@users.noreply.github.com>
2024-03-07 20:04:04 -0500

    completed leetcode 27

--- leetcode/leetcode-27.js
+++ leetcode/leetcode-27.js
@@ -4,5 +4,12 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
-    
+    let pointer = 0
+    let swap = nums.length - 1
+
+    while (pointer < swap) {
+        if (nums[pointer] === val) {
+            
+        }
+    }
 };
