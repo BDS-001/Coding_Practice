3bde79b57e0c5ab3232ffff9334a3806c870089b
BDS <113209967+BDS-001@users.noreply.github.com>
2024-03-14 20:08:27 -0400

    added fuync to clean array buy sorting and removing duplicates

--- odin_project/Project-binaryTree/binaryTree.js
+++ odin_project/Project-binaryTree/binaryTree.js
@@ -5,3 +5,23 @@ const node = function(data=null, left=null, right=null) {
         right: right,
     }
 }
+
+class Tree {
+    constructor(array) {
+        this.tree = this.buildTree(array)
+        this.root = null
+    }
+
+    _cleanArray(array) {
+        array.sort((a, b) => a - b);
+        return [...new Set(array)] 
+    }
+
+    buildTree(array) {
+        array = this._cleanArray(array)
+        return array
+    }
+}
+
+const test = new Tree([5,7,8,65,2,67,4,3,234564,56,2342,564,234,654,3])
+console.log(test.tree)
