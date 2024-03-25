ebfb5dc8338070473507b42f41d3623e708b34c4
BDS <113209967+BDS-001@users.noreply.github.com>
2024-03-25 13:18:46 -0400

    added const variables to hold the contraint data

--- odin_project/Project-knightsTravails/knightsTravails.js
+++ odin_project/Project-knightsTravails/knightsTravails.js
@@ -1,3 +1,24 @@
 function knightMoves(start, finish) {
+    if (start[0] === finish[0] && start[1] === finish[1]) return start
+
+    const minCoordinate = 0
+    const maxCoordinate = 7
+    const knightMoves = [
+        [2, 1],  // Move 2 squares up, 1 square right
+        [2, -1], // Move 2 squares up, 1 square left
+        [-2, 1], // Move 2 squares down, 1 square right
+        [-2, -1],// Move 2 squares down, 1 square left
+        [1, 2],  // Move 1 square up, 2 squares right
+        [1, -2], // Move 1 square up, 2 squares left
+        [-1, 2], // Move 1 square down, 2 squares right
+        [-1, -2] // Move 1 square down, 2 squares left
+    ];
+    
+    const path = function(current, goal, path=[]) {
+
+
+    }
     return []
 }
+
+
