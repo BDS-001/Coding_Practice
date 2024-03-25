f699105205104c223de81b48200448cd0572ca7d
BDS <113209967+BDS-001@users.noreply.github.com>
2024-03-25 14:07:57 -0400

    knightsMoves function now outputs the shortest path to the goal

--- odin_project/Project-knightsTravails/knightsTravails.js
+++ odin_project/Project-knightsTravails/knightsTravails.js
@@ -1,6 +1,4 @@
 function knightMoves(start, finish) {
-    if (start[0] === finish[0] && start[1] === finish[1]) return start
-
     const minCoordinate = 0
     const maxCoordinate = 7
     const knightMoves = [
@@ -13,12 +11,51 @@ function knightMoves(start, finish) {
         [-1, 2], // Move 1 square down, 2 squares right
         [-1, -2] // Move 1 square down, 2 squares left
     ];
-    
-    const path = function(current, goal, path=[]) {
 
+    const isValid = function(path, newPosition) {
+        //check if the new position goes outside of the board
+        if (newPosition[0] > maxCoordinate) return false
+        if (newPosition[0] < minCoordinate ) return false
+        if (newPosition[1] > maxCoordinate) return false
+        if (newPosition[1] < minCoordinate) return false
 
+        //check if the postion ha already been visited in the path
+        for (let i = 0; i < path.length; i++) {
+            if(path[i][0] === newPosition[0] && path[i][1] === newPosition[1]) return false
+        }
+        return true
     }
-    return []
+
+    const outputPath = function(path) {
+        console.log(`You made it in ${path.length - 1} move(s)!  Here's your path:`)
+        for (let i = 0; i < path.length; i++) {
+            console.log(path[i])
+        }
+    }
+
+    //start the queue with the starting position
+    const queue = [[start]]
+    while (queue.length > 0) {
+        //get the latest path in the queue, get the latest coordinate from the path 
+        const currentPath = queue.shift()
+        const currentPosition = currentPath[currentPath.length - 1]
+        if (currentPosition[0] === finish[0] && currentPosition[1] === finish[1]){
+            outputPath(currentPath)
+            return currentPath
+        };
+
+        //add a new path for each possible position
+        for (let i = 0; i < knightMoves.length; i++) {
+            const newPosition = [currentPosition[0] + knightMoves[i][0], currentPosition[1] + knightMoves[i][1]]
+            if (isValid(currentPath, newPosition)) {
+                const newPath = Array.from(currentPath)
+                newPath.push(newPosition)
+                queue.push(newPath)
+            }
+        }
+    }
 }
 
 
+knightMoves([0,0],[3,3])
+knightMoves([0,0],[7,7])
