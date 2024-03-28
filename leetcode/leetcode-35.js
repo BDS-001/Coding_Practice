8a6beb2187148c78ff52420acaa8709d7d066dfd
BDS <113209967+BDS-001@users.noreply.github.com>
2024-03-28 19:29:55 -0400

    completed leetcode 35

--- leetcode/leetcode-35.js
+++ leetcode/leetcode-35.js
@@ -4,5 +4,19 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-    
+    let start = 0
+    let end = nums.length
+
+    while (true) {
+        let index = Math.floor((start + end)/2)
+        if (nums[index] === target) return index
+        if (end - start < 2) {
+            if (nums[index] > target) return index
+            return index + 1
+        }
+        if (nums[index] > target) end = index
+        if (nums[index] < target) start = index
+    }
 };
+
+console.log(searchInsert([1,3,5,6], 7))
