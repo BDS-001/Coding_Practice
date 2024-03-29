98563cc2270fadcb2696ed5c34fb289f5fb96d7f
BDS <113209967+BDS-001@users.noreply.github.com>
2024-03-29 13:37:43 -0400

    completed leetcode 100

--- leetcode/leetcode-100.js
+++ leetcode/leetcode-100.js
@@ -12,5 +12,28 @@
  * @return {boolean}
  */
 var isSameTree = function(p, q) {
-    
+    if ((q.val != p.val)) return false
+
+    const pQueue = [p]
+    const qQueue = [q]
+
+    while (pQueue.length > 0 && qQueue.length > 0) {
+        const currentP = pQueue.shift()
+        const currentQ = qQueue.shift()
+        if ((!currentP.left && currentQ.left) || (currentP.left && !currentQ.left)) return false;
+        if ((!currentP.right && currentQ.right) || (currentP.right && !currentQ.right)) return false;
+        
+        if (currentP.left) {
+            if (currentP.left.val != currentQ.left.val) return false
+            pQueue.push(currentP.left)
+            qQueue.push(currentQ.left)
+        }
+        if (currentP.right) {
+            if (currentP.right.val != currentQ.right.val) return false
+            pQueue.push(currentP.right)
+            qQueue.push(currentQ.right)
+        }
+
+    }
+    return true
 };
