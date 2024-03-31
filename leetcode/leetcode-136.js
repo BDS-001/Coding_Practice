07c56443a92c4a3bd8204169b5398d58b12cee34
BDS <113209967+BDS-001@users.noreply.github.com>
2024-03-31 13:13:16 -0400

    completed leetcode 136

--- leetcode/leetcode-136.js
+++ leetcode/leetcode-136.js
@@ -4,4 +4,11 @@
  */
 var singleNumber = function(nums) {
     if (nums.length === 1) return nums[0]
+
+    let xor = 0
+    for (let i = 0; i < nums.length; i++) {
+        xor ^= nums[i]
+    }
+    return xor
 };
+
