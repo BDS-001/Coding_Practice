a3c18997ee501c53530be80b09d4c2d0271f4570
BDS <113209967+BDS-001@users.noreply.github.com>
2024-04-01 21:24:56 -0400

    completed leetcode 171

--- leetcode/leetcode-171.js
+++ leetcode/leetcode-171.js
@@ -3,5 +3,11 @@
  * @return {number}
  */
 var titleToNumber = function(columnTitle) {
-    
+    let counter = 1
+    let column = 0
+    for (let i = columnTitle.length - 1; i >= 0; i--) {
+        column += (columnTitle.charCodeAt(i) - 64) * counter;
+        counter *= 26;
+    }
+    return column
 };
