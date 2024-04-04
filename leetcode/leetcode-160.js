f21ddd8aaec4b2cb86b6af83a9eddfdf04012d6e
BDS <113209967+BDS-001@users.noreply.github.com>
2024-04-03 21:38:15 -0400

    completed leetcode 160

--- leetcode/leetcode-160.js
+++ leetcode/leetcode-160.js
@@ -12,5 +12,22 @@
  * @return {ListNode}
  */
 var getIntersectionNode = function(headA, headB) {
-    
+    let hashMap = new Map();
+
+    let pointerA = headA
+    let pointerB = headB
+    while(pointerA || pointerB) {
+        if (hashMap.has(pointerA)) return pointerA
+        if (pointerA) {
+            hashMap.set(pointerA, 0)
+            pointerA = pointerA.next
+        }
+        
+        if (hashMap.has(pointerB)) return pointerB
+        if (pointerB) {
+            hashMap.set(pointerB, 0)
+            pointerB = pointerB.next
+        }
+    }
+    return null
 };
