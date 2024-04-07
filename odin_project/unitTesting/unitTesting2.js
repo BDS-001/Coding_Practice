455e4e3294fca48824f474f9da2e493d2a3415e5
BDS <113209967+BDS-001@users.noreply.github.com>
2024-04-07 15:00:35 -0400

    practice unit testing

--- odin_project/unitTesting/unitTesting2.js
+++ odin_project/unitTesting/unitTesting2.js
@@ -1,5 +1,20 @@
 if (orderTotal({
     items: [
+        {name: 'Dragon Food', price: 8},
+        {name: 'Dragonm cage (small)', price: 800}
+    ]
+}) !== 808) {
+    throw new Error('check fail: Happy path(1)')
+    //happy path is main path with no edge cases
+}
+
+//should onyl write functions for what the test is testing, returning the value will pass all the tests, one test is not enough
+let orderTotal = function() {
+     return 808
+}
+
+if (orderTotal({
+    items: [
         {name: 'Dragon Food', price: 8, quantity: 1},
         {name: 'Dragonm cage (small)', price: 800, quantity: 1}
     ]
@@ -8,7 +23,33 @@ if (orderTotal({
     //happy path is main path with no edge cases
 }
 
-//should onyl write functions for what the test is testing, returning the value will pass the test
-function orderTotal() {
-     return 808
+
+
+
+
+//triangulation test first methos is writting multiple tests
+if (orderTotal({
+    items: [
+        {name: 'Dragon collar', price: 20},
+        {name: 'Dragonm chew toy', price: 40}
+    ]
+}) !== 60) {
+    throw new Error('check fail: Happy path(2)')
+    //happy path is main path with no edge cases
 }
+
+if (orderTotal({
+    items: [
+        {name: 'Dragon bed', price: 65},
+        {name: 'Dragonm leash', price: 130}
+    ]
+}) !== 195) {
+    throw new Error('check fail: Happy path(3)')
+    //happy path is main path with no edge cases
+}
+
+//update order total to pass all tests
+orderTotal = function(order) {
+    return order.items.reduce((prev, cur) => cur.price + prev, 0)
+}
+
