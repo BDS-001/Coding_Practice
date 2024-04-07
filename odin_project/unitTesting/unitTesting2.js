857467653a6fdbd6b188db3137d159a7fb70f2a4
BDS <113209967+BDS-001@users.noreply.github.com>
2024-04-07 15:14:48 -0400

    practice test casees when updating api

--- odin_project/unitTesting/unitTesting2.js
+++ odin_project/unitTesting/unitTesting2.js
@@ -4,8 +4,7 @@ if (orderTotal({
         {name: 'Dragonm cage (small)', price: 800}
     ]
 }) !== 808) {
-    throw new Error('check fail: Happy path(1)')
-    //happy path is main path with no edge cases
+    throw new Error('check fail: quantity')
 }
 
 //should onyl write functions for what the test is testing, returning the value will pass all the tests, one test is not enough
@@ -19,8 +18,7 @@ if (orderTotal({
         {name: 'Dragonm cage (small)', price: 800, quantity: 1}
     ]
 }) !== 808) {
-    throw new Error('check fail: Happy path')
-    //happy path is main path with no edge cases
+    throw new Error('check fail: quantity')
 }
 
 
@@ -34,8 +32,7 @@ if (orderTotal({
         {name: 'Dragonm chew toy', price: 40}
     ]
 }) !== 60) {
-    throw new Error('check fail: Happy path(2)')
-    //happy path is main path with no edge cases
+    throw new Error('check fail: quantity')
 }
 
 if (orderTotal({
@@ -44,11 +41,27 @@ if (orderTotal({
         {name: 'Dragonm leash', price: 130}
     ]
 }) !== 195) {
-    throw new Error('check fail: Happy path(3)')
-    //happy path is main path with no edge cases
+    throw new Error('check fail: quantity')
 }
 
 //update order total to pass all tests
 orderTotal = function(order) {
     return order.items.reduce((prev, cur) => cur.price + prev, 0)
 }
+
+//to update the code to include quantity we have to be mindful of the prev tests, chanignt the api will break all the tests and any uses of it in the wild
+orderTotal = function(order) {
+    return order.items.reduce((prev, cur) => cur.price * (cur.quantity || 1) + prev, 0)
+}
+
+if (orderTotal({
+    items: [
+        {name: 'Dragon bed', price: 65, quantity: 3},
+        {name: 'Dragonm leash', price: 130, quantity: 4}
+    ]
+}) !== 715) {
+    throw new Error('check fail: Happy path')
+    //happy path is main path with no edge cases
+}
+
+//all older tests now test qunaity missing
