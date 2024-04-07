5fee570901d6b42428968c0454821cdda0145fb5
BDS <113209967+BDS-001@users.noreply.github.com>
2024-04-06 20:48:22 -0400

    completed leetcode 23

--- leetcode/leetcode-23.js
+++ leetcode/leetcode-23.js
@@ -1,14 +1,36 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
+
+ //Definition for singly-linked list.
+function ListNode(val, next) {
+    this.val = (val===undefined ? 0 : val)
+    this.next = (next===undefined ? null : next)
+}
+ 
 /**
  * @param {ListNode[]} lists
  * @return {ListNode}
  */
 var mergeKLists = function(lists) {
+    const newList = new ListNode()
+    let pointer = newList
+    while(lists.length > 0) {
+        let lowest = { node: null, pos: null, val: Infinity }
 
+        //get the lowest value
+        for (let i = 0; i < lists.length; i++) {
+            if (lists[i] && (lowest.pos === null || lowest.val > lists[i].val)) {
+                lowest = { node: lists[i], pos: i, val: lists[i].val }
+            }
+        }
+        if (lowest.pos !== null) {
+            pointer.next = lowest.node
+            pointer = pointer.next
+            lists[lowest.pos] = lists[lowest.pos].next
+            if (!lists[lowest.pos]) {
+                lists.splice(lowest.pos, 1)
+            }
+        } else {
+            break
+        }
+    }
+    return newList.next
 };
