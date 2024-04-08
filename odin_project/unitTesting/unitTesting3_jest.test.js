23f05b16ed6b6dacb75b37d6d6fdbdc367a0ec8d
BDS <113209967+BDS-001@users.noreply.github.com>
2024-04-08 19:07:44 -0400

    rewrote all manual tests using jest syntax

--- odin_project/unitTesting/unitTesting3_jest.test.js
+++ odin_project/unitTesting/unitTesting3_jest.test.js
@@ -8,47 +8,51 @@ test('adds 1 + 2 to equal 3', () => {
     expect(1 + 2).toBe(3);
   });
 
-if (orderTotal({
-    items: [
-        {name: 'Dragon Food', price: 8},
-        {name: 'Dragonm cage (small)', price: 800}
-    ]
-}) !== 808) {
-    throw new Error('check fail: quantity')
-}
 
-if (orderTotal({
-    items: [
-        {name: 'Dragon Food', price: 8, quantity: 1},
-        {name: 'Dragonm cage (small)', price: 800, quantity: 1}
-    ]
-}) !== 808) {
-    throw new Error('check fail: quantity')
-}
 
-if (orderTotal({
-    items: [
-        {name: 'Dragon collar', price: 20},
-        {name: 'Dragonm chew toy', price: 40}
-    ]
-}) !== 60) {
-    throw new Error('check fail: quantity')
-}
 
-if (orderTotal({
-    items: [
-        {name: 'Dragon bed', price: 65},
-        {name: 'Dragonm leash', price: 130}
-    ]
-}) !== 195) {
-    throw new Error('check fail: quantity')
-}
+//rewritting tests
+it('No Quantity 1', () => {
+    expect(orderTotal({
+        items: [
+            {name: 'Dragon Food', price: 8},
+            {name: 'Dragon cage (small)', price: 800}
+        ]
+    })).toBe(808);
+});
 
-if (orderTotal({
-    items: [
-        {name: 'Dragon bed', price: 65, quantity: 3},
-        {name: 'Dragonm leash', price: 130, quantity: 4}
-    ]
-}) !== 715) {
-    throw new Error('check fail: Happy path')
-}
+it('Quantity', () => {
+    expect(orderTotal({
+        items: [
+            {name: 'Dragon Food', price: 8, quantity: 1},
+            {name: 'Dragon cage (small)', price: 800, quantity: 1}
+        ]
+    })).toBe(808);
+});
+
+it('No Quantity 2', () => {
+    expect(orderTotal({
+        items: [
+            {name: 'Dragon collar', price: 20},
+            {name: 'Dragon chew toy', price: 40}
+        ]
+    })).toBe(60);
+});
+
+it('No Quantity 3', () => {
+    expect(orderTotal({
+        items: [
+            {name: 'Dragon bed', price: 65},
+            {name: 'Dragon leash', price: 130}
+        ]
+    })).toBe(195);
+});
+
+it('Happy Path', () => {
+    expect(orderTotal({
+        items: [
+            {name: 'Dragon bed', price: 65, quantity: 3},
+            {name: 'Dragon leash', price: 130, quantity: 4}
+        ]
+    })).toBe(715);
+});
