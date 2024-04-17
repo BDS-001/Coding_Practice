d6b44650fde4af56de4e195df0160284aaf692a7
BDS <113209967+BDS-001@users.noreply.github.com>
2024-04-16 21:23:55 -0400

    adde referrences in comments

--- odin_project/unitTesting/MOC/MOC-UnitTesting.js
+++ odin_project/unitTesting/MOC/MOC-UnitTesting.js
@@ -1,3 +1,6 @@
+// https://www.youtube.com/watch?v=3PjdxjWK0F0
+// https://www.theodinproject.com/lessons/node-path-javascript-more-testing
+
 function orderTotal(order) {
     return order.items.reduce((prev,cur) => cur.price * (cur.quantity || 1) + prev, 0)
 }
