c37eb488da1740118650be7c474667eedeb9cf28
BDS <113209967+BDS-001@users.noreply.github.com>
2024-04-24 19:27:02 -0400

    current version exceeds time limit

--- leetcode/leetcode-16.js
+++ leetcode/leetcode-16.js
@@ -4,5 +4,21 @@
  * @return {number}
  */
 var threeSumClosest = function(nums, target) {
-    
+    let res = Infinity
+    const recursion = (numsArray, currentSet, count) => {
+        if (res === target) return
+        if (count === 3) {
+            let value = currentSet.reduce((acc, num) => acc + num, 0)
+            if (Math.abs(value - target) < Math.abs(res - target)) res = value
+        }
+
+        for (let i = 0; i < numsArray.length; i++) {
+            let newCurrentSet = [...currentSet, numsArray[i]];
+            let newNums = [...numsArray]
+            newNums.splice(i, 1)
+            recursion(newNums, newCurrentSet, count+1)
+        }
+    }
+    recursion(nums, [], 0)
+    return res
 };
