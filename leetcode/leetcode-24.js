090beb4e0429fa6469a28697f3a6156b32277117
BDS <113209967+BDS-001@users.noreply.github.com>
2024-04-27 20:06:51 -0400

    leetcode 15 starter code

--- leetcode/leetcode-24.js
+++ leetcode/leetcode-24.js
@@ -10,5 +10,21 @@
  * @return {ListNode}
  */
 var swapPairs = function(head) {
-    
+    if (!head || !head.next) return head
+    pointer1 = head
+    pointer2 = pointer1.next
+    prev = null
+    head = pointer2
+
+    while (pointer1 && pointer2) {
+        pointer1.next = pointer2.next
+        pointer2.next = pointer1
+
+        if (prev) prev.next = pointer2
+        prev = pointer1
+
+        pointer1 = pointer1.next
+        if (pointer1) pointer2 = pointer1.next
+    }
+    return head
 };
