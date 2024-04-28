af5e52661a7a29f6a845f40011ad5ae462948dd1
BDS <113209967+BDS-001@users.noreply.github.com>
2024-04-27 20:39:09 -0400

    conpleted leetcode 15

--- leetcode/leetcode-15.js
+++ leetcode/leetcode-15.js
@@ -3,5 +3,30 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    
+    const res = []
+    nums.sort((a, b) => a - b)
+    for (let i = 0; i < nums.length - 1; i++) {
+        if (i > 0 && nums[i] === nums[i - 1]) {
+            continue;
+        }
+        let left = i + 1
+        let right = nums.length - 1
+        while(left < right) {
+            const total = nums[i] + nums[left] + nums[right]
+            if (total === 0){
+                res.push([nums[i], nums[left], nums[right]])
+                while (left < right && nums[left] === nums[left + 1]) {
+                    left++;
+                }
+                while (left < right && nums[right] === nums[right - 1]) {
+                    right--;
+                }
+                left++;
+                right--;
+            } else {
+                (total > 0) ? right-- : left++
+            }
+        }
+    }
+    return res
 };
