51b590bd5ca7d36d6b5ca93a99a2546619041acd
BDS <113209967+BDS-001@users.noreply.github.com>
2024-04-29 11:33:03 -0400

    moc unit test comaptable with async function

---
+++ odin_project/unitTesting/MOC/moc.test.js
@@ -0,0 +1,56 @@
+const orderTotal = require('./unitTesting3_function')
+
+//test if jest is working correctly, file must end in .test.js
+it('works', () => {
+
+})
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(1 + 2).toBe(3);
+  });
+
+//rewritting tests
+it('No Quantity 1', () => {
+    orderTotal({
+        items: [
+            {name: 'Dragon Food', price: 8},
+            {name: 'Dragon cage (small)', price: 800}
+        ]
+    }).then(result => expect(result).toBe(808));
+});
+
+it('Quantity', () => {
+    orderTotal({
+        items: [
+            {name: 'Dragon Food', price: 8, quantity: 1},
+            {name: 'Dragon cage (small)', price: 800, quantity: 1}
+        ]
+    }).then(result => expect(result).toBe(808));
+});
+
+it('No Quantity 2', () => {
+    orderTotal({
+        items: [
+            {name: 'Dragon collar', price: 20},
+            {name: 'Dragon chew toy', price: 40}
+        ]
+    }).then(result => expect(result).toBe(60));
+});
+
+it('No Quantity 3', () => {
+    orderTotal({
+        items: [
+            {name: 'Dragon bed', price: 65},
+            {name: 'Dragon leash', price: 130}
+        ]
+    }).then(result => expect(result).toBe(195));
+});
+
+it('Happy Path', () => {
+    orderTotal({
+        items: [
+            {name: 'Dragon bed', price: 65, quantity: 3},
+            {name: 'Dragon leash', price: 130, quantity: 4}
+        ]
+    }).then(result => expect(result).toBe(715));
+});
