e953a8784a00f73dcfbe21f1443dbe16e810e35b
BDS <113209967+BDS-001@users.noreply.github.com>
2024-04-30 21:11:58 -0400

    completed leetcode 39

--- leetcode/leetcode-39.py
+++ leetcode/leetcode-39.py
@@ -1,3 +1,26 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         
+        answers = []
+        
+        def check(current):
+            total = 0
+            for num1 in current:
+                total += num1
+            if total == target:
+                for item in answers:
+                    if sorted(current) == sorted(item):
+                        return
+                answers.append(current)
+                return
+            if total > target:
+                return
+            
+            for num in candidates:
+                tmp = current + [num]
+                check(tmp)
+                
+                
+        test = []
+        check(test)
+        return answers
