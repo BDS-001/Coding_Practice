3239b845602372e57fd0e31bd3d76b3ebd2c7cb5
BDS <113209967+BDS-001@users.noreply.github.com>
2024-06-25 20:15:27 -0400

    completed leetcode second solution

--- leetcode/leetcode-169.js
+++ leetcode/leetcode-169.js
@@ -15,3 +15,21 @@ var majorityElement = function(nums) {
 
     return Object.entries(dict).sort((a, b) => b[1] - a[1])[0][0]
 };
+
+var majorityElement = function(nums) {
+    if (nums.length === 1) return nums[0]
+    const newNums = nums.sort((a,b) => b - a)
+    let highest = [null, 0]
+    let currentNum = [nums[0], 0]
+
+    for (let i = 0; i < newNums.length; i++) {
+        if (newNums[i] != currentNum[0]) {
+            if (currentNum[1] > highest[1]) highest = currentNum
+            currentNum = [newNums[i], 1]
+            continue
+        }
+        currentNum[1] += 1
+    }
+
+    return highest[1]
+};
