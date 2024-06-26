d41b611e1a2e7ceaa18610449515df56eb3717e8
BDS <113209967+BDS-001@users.noreply.github.com>
2024-06-25 20:16:35 -0400

    completed leetcode second solution

--- leetcode/leetcode-169.js
+++ leetcode/leetcode-169.js
@@ -20,9 +20,9 @@ var majorityElement = function(nums) {
     if (nums.length === 1) return nums[0]
     const newNums = nums.sort((a,b) => b - a)
     let highest = [null, 0]
-    let currentNum = [nums[0], 0]
+    let currentNum = [nums[0], 1]
 
-    for (let i = 0; i < newNums.length; i++) {
+    for (let i = 1; i < newNums.length; i++) {
         if (newNums[i] != currentNum[0]) {
             if (currentNum[1] > highest[1]) highest = currentNum
             currentNum = [newNums[i], 1]
@@ -31,5 +31,5 @@ var majorityElement = function(nums) {
         currentNum[1] += 1
     }
 
-    return highest[1]
+    return currentNum[1] > highest[1] ? currentNum[0] : highest[0]
 };
