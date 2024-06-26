233ac23bea60ce316dceda0763cdf9b470acf292
BDS <113209967+BDS-001@users.noreply.github.com>
2024-06-25 20:01:05 -0400

    completed leetcode 169

--- leetcode/leetcode-169.js
+++ leetcode/leetcode-169.js
@@ -3,5 +3,15 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
+    const dict = {}
 
+    for (let i = 0; i < nums.length; i++) {
+        if (dict[nums[i]]) {
+            dict[nums[i]] += 1
+            continue
+        }
+        dict[nums[i]] = 1
+    }
+
+    return Object.entries(dict).sort((a, b) => b[1] - a[1])[0][0]
 };
