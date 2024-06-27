a1d5a82d46dfa42d88eded99789219d9bd61b1b9
BDS <113209967+BDS-001@users.noreply.github.com>
2024-06-26 23:17:50 -0400

    completed leetcode 268

--- leetcode/leetcode-268.js
+++ leetcode/leetcode-268.js
@@ -3,5 +3,10 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
+    const newNums = nums.sort((a, b) => a - b);
 
+    for (let i = 0; i < newNums.length; i++) {
+        if (newNums[i] != i) return i
+    }
+    return newNums.length
 };
