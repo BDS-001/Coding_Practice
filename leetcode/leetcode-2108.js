b201fabf6b518278ad27e894129fd619607578ef
BDS <113209967+BDS-001@users.noreply.github.com>
2024-06-27 17:50:11 -0400

    completed leetcode 2108

--- leetcode/leetcode-2108.js
+++ leetcode/leetcode-2108.js
@@ -3,5 +3,17 @@
  * @return {string}
  */
 var firstPalindrome = function(words) {
-    
+    function palindrome(word) {
+        let reverse = word.length - 1
+        for (let i = 0; i < word.length; i++) {
+            if (word[i] != word[reverse]) return false
+            reverse -= 1
+        }
+        return true
+    }
+
+    for (let i = 0; i < words.length; i++) {
+        if (palindrome(words[i])) return words[i]
+    }
+    return ''
 };
