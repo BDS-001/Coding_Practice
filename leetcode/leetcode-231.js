0179a1dd9b2f66c3a8fc6459bb465becb947d6dc
BDS <113209967+BDS-001@users.noreply.github.com>
2024-06-27 18:09:29 -0400

    alternate leetcode 231 solution

--- leetcode/leetcode-231.js
+++ leetcode/leetcode-231.js
@@ -13,3 +13,13 @@ var isPowerOfTwo = function(n) {
     }
     return false
 };
+
+var isPowerOfTwo = function(n) {
+    if (n === 0) return false
+
+    while (n !== 1) {
+        if (n%2 != 0) return false
+        n /= 2
+    }
+    return true
+};
