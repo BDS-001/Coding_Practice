dcf369f5ef8cd33ce5a50880c548411aef7a47a8
BDS <113209967+BDS-001@users.noreply.github.com>
2024-06-29 21:59:49 -0400

    completed leetcode 2540

--- leetcode/leetcode-2540.js
+++ leetcode/leetcode-2540.js
@@ -4,5 +4,19 @@
  * @return {number}
  */
 var getCommon = function(nums1, nums2) {
-    
+    let p1 = 0
+    let p2 = 0
+    const max1 = nums1.length - 1
+    const max2 = nums2.length - 1
+
+    while(p1 <= max1 && p2 <= max2) {
+        if (nums1[p1] === nums2[p2]) return nums1[p1]
+        if (p1 === max1) {
+            p2++
+        } else if (p2 === max2) {
+            p1++
+        } else {
+            nums1[p1] < nums2[p2] ? p1++ : p2++
+        }
+    }
 };
