ec89da36df9b3f2e7bfab1b933401d2b12669f99
BDS <113209967+BDS-001@users.noreply.github.com>
2024-07-05 16:47:44 -0400

    time limit exceeded

--- leetcode/leetcode-1481.js
+++ leetcode/leetcode-1481.js
@@ -4,5 +4,18 @@
  * @return {number}
  */
 var findLeastNumOfUniqueInts = function(arr, k) {
-    
+    const hash = {}
+    for (let i = 0; i < arr.length; i++) {
+        hash[arr[i]] ? hash[arr[i]] += 1 : hash[arr[i]]
+    }
+
+    const sortedArr = Object.entries(hash).sort(([, valueA], [, valueB]) => valueA - valueB)
+    while (true) {
+        if (k > sortedArr[0][1]) {
+            k -= sortedArr[0][1]
+            sortedArr.shift()
+        } else {
+            return sortedArr.length
+        }
+    }
 };
