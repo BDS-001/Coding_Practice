b325f7dac1bb092ae865b65642c63dd2fbfeed84
BDS <113209967+BDS-001@users.noreply.github.com>
2024-07-05 16:12:11 -0400

    completed leetcode 451

--- leetcode/leetcode-451.js
+++ leetcode/leetcode-451.js
@@ -3,5 +3,16 @@
  * @return {string}
  */
 var frequencySort = function(s) {
-    
+    let map = {}
+    for (let i = 0; i < s.length; i++) {
+        map[s[i]] ? map[s[i]] += 1 : map[s[i]] = 1
+    }
+    const vals = Object.entries(map)
+    vals.sort(([, valueA], [, valueB]) => valueB - valueA)
+
+    let res = ''
+    for (let i = 0; i < vals.length; i++) {
+        res += vals[i][0].repeat(vals[i][1])
+    }
+    return res
 };
