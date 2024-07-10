d55a4e730d9b06a34004331139449f0c3c923eed
BDS <113209967+BDS-001@users.noreply.github.com>
2024-07-09 21:30:11 -0400

    completed leetcode 1614

--- leetcode/leetcode-1614.js
+++ leetcode/leetcode-1614.js
@@ -3,5 +3,15 @@
  * @return {number}
  */
 var maxDepth = function(s) {
-    
+    let depth = 0
+    let max = 0
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '(') {
+            depth += 1
+            max = Math.max(max, depth)
+        }
+        if (s[i] === ')') depth -= 1
+    }
+    return max
 };
