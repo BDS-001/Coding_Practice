32511363773055706bb95cffd18cd59d5fc58d4c
BDS <113209967+BDS-001@users.noreply.github.com>
2024-07-15 20:50:32 -0400

    completed leetcode 205

--- leetcode/leetcode-205.js
+++ leetcode/leetcode-205.js
@@ -4,5 +4,18 @@
  * @return {boolean}
  */
 var isIsomorphic = function(s, t) {
-    
+    const map = new Map
+    const used = new Map
+    for (let i = 0; i < s.length; i++) {
+        if (!map.has(s[i])) {
+            if (used.has(t[i])) {
+                return false;
+            }
+            map.set(s[i], t[i])
+            used.set(t[i], true)
+        } else {
+            if (map.get(s[i]) != t[i]) return false
+        }
+    }
+    return true
 };
