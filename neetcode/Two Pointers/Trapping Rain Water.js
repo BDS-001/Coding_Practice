0f89a50b2c48f02de4664fd5437bd9b214b5a221
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-09-04 20:22:06 -0400

    first attempt

--- neetcode/Two Pointers/Trapping Rain Water.js
+++ neetcode/Two Pointers/Trapping Rain Water.js
@@ -3,5 +3,25 @@ class Solution {
      * @param {number[]} height
      * @return {number}
      */
-    trap(height) {}
+    trap(heights) {
+        let left = 0
+        let right = heights.length - 1
+        let maxRain = 0
+
+        while(left < right) {
+            const height = Math.min(heights[left], heights[right])
+            let totalRain = 0
+            for (let i = left + 1; i < right; i++) {
+                totalRain += Math.max(0, height - heights[i])
+            }
+            
+            maxRain = Math.max(maxRain, totalRain)
+            if (heights[left] > heights[right]) {
+                right --;
+            } else {
+                left ++;
+            }
+        }
+        return maxRain
+    }
 }
