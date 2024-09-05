0c1a5dfeb81bc2a646accd35c37cf19ed7ad90e7
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-09-04 20:11:05 -0400

    completed problem

--- neetcode/Two Pointers/Max Water Container.js
+++ neetcode/Two Pointers/Max Water Container.js
@@ -3,5 +3,21 @@ class Solution {
      * @param {number[]} heights
      * @return {number}
      */
-    maxArea(heights) {}
+    maxArea(heights) {
+        let left = 0
+        let right = heights.length - 1
+        let maxWater = 0
+
+        while(left < right) {
+            const height = Math.min(heights[left], heights[right])
+            const water = height * (right - left)
+            maxWater = Math.max(maxWater, water)
+            if (heights[left] > heights[right]) {
+                right --;
+            } else {
+                left ++;
+            }
+        }
+        return maxWater
+    }
 }
