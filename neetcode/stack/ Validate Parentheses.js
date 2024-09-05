c8436539ebaf0bdc91a5ebc34fb123fe17725b4c
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-09-05 19:01:29 -0400

    completed neetcode

--- neetcode/stack/ Validate Parentheses.js
+++ neetcode/stack/ Validate Parentheses.js
@@ -3,5 +3,22 @@ class Solution {
      * @param {string} s
      * @return {boolean}
      */
-    isValid(s) {}
+    isValid(s) {
+        const map = {
+            ']': '[',
+            '}': '{',
+            ')': '('
+        }
+
+        const stack = []
+        for (let i = 0; i < s.length; i++) {
+            if (!map[s[i]]) {
+                stack.unshift(s[i])
+            } else {
+                const par = stack.shift()
+                if (par != map[s[i]]) return false
+            }
+        }
+        return stack.length > 0 ? false : true
+    }
 }
