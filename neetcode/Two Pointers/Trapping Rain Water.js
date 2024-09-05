b8cef72c5df0e487d5fd0e1486e3af57329e9c18
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-09-05 17:09:01 -0400

    rain attempt

--- neetcode/Two Pointers/Trapping Rain Water.js
+++ neetcode/Two Pointers/Trapping Rain Water.js
@@ -5,32 +5,46 @@ class Solution {
      */
     trap(heights) {
         let left = 0
-        let right = left + 1
-        let collect = false
+        let right = null
         let maxRain = 0
 
-        while(right < heights.length) {
-            console.log('index:' , left, right, 'values:', heights[left], heights[right], 'max:', maxRain)
-            if (heights[left] === 0) {
-                //move until left is not 0
+        while(left < heights.length) {
+            if (heights[left] <= 0) {
                 left ++;
-                right = left + 1
-            } else if (heights[right] > heights[right - 1] && !collect) {
-                // once right starts to increase set collect to true
-                collect = true;
-            } else if (collect && heights[right] < heights[right - 1]) {
-                // if right starts to decrease while collect is active, collec the water and reset
-                console.log('hit')
-                const maxHeight = Math.min(heights[left], heights[right - 1])
-                for (let i = left + 1; i < right - 1; i++) {
-                    maxRain += maxHeight - heights[i]
-                    console.log(maxHeight - heights[i])
+                continue
+            }
+
+            let maxHeight = null
+            let minHeight = null
+            for (let i = left + 1; i < heights.length; i++) {
+                maxHeight ? maxHeight = heights[i] : maxHeight = Math.max(heights[i], maxHeight)
+                minHeight ? minHeight = heights[i] : minHeight = Math.min(minHeight, heights[i])
+                if (heights[i] >= heights[left]) {
+                    right = i
+                    break
                 }
-                left = right - 1
-                collect = false
+            }
+
+            if (!right && minHeight < maxHeight) {
+                for (let i = left + 1; i < heights.length; i++) {
+                    if (heights[i] === maxHeight) {
+                        right = i
+                        break
+                    }
+                }
+            }
+
+            if (right) {
+                const height = Math.min(heights[left] , heights[right])
+                console.log('hright', height, 'left', left, 'right', right)
+                for (let i = left + 1; i < right; i++) {
+                    maxRain += height - heights[i]
+                    
+                }
+                left = right
+                right = null
             } else {
-                //increment right
-                right ++;
+                left++;
             }
         }
         return maxRain
@@ -41,3 +55,6 @@ const test1 = new Solution().trap([0,1,0,2,1,0,1,3,2,1,2,1])
 const test2 = new Solution().trap([4,2,0,3,2,5])
 console.log(test1, test1 === 6)
 console.log(test2, test2 === 9)
+
+const test3 = new Solution().trap([4,2,3])
+console.log(test3, test3 === 1)
