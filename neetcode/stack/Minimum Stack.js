81c68e033597a439b6ff8bb25d27ea8ead1f3545
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-09-06 15:10:18 -0400

    completed solution

--- neetcode/stack/Minimum Stack.js
+++ neetcode/stack/Minimum Stack.js
@@ -1,24 +1,37 @@
 class MinStack {
-    constructor() {}
+    constructor() {
+        this.min = []
+        this.stack = []
+    }
 
     /**
      * @param {number} val
      * @return {void}
      */
-    push(val) {}
+    push(val) {
+        this.stack.unshift(val)
+        this.min.unshift(Math.min(val, this.min.length === 0 ? val : this.min[0]))
+    }
 
     /**
      * @return {void}
      */
-    pop() {}
+    pop() {
+        this.stack.shift()
+        this.min.shift()
+    }
 
     /**
      * @return {number}
      */
-    top() {}
+    top() {
+        return this.stack[0]
+    }
 
     /**
      * @return {number}
      */
-    getMin() {}
+    getMin() {
+        return this.min[0]
+    }
 }
