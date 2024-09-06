ab2b61bf256da215ab65bea5eef01289652bfe7e
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-09-06 17:06:35 -0400

    completed problem

--- neetcode/stack/Evaluate Reverse Polish Notation.js
+++ neetcode/stack/Evaluate Reverse Polish Notation.js
@@ -3,5 +3,30 @@ class Solution {
      * @param {string[]} tokens
      * @return {number}
      */
-    evalRPN(tokens) {}
+    evalRPN(tokens) {
+        let nums = []
+        for (let i = 0; i < tokens.length; i++) {
+            let val = parseInt(tokens[i])
+            if (val || val === 0) {
+                nums.push(val)
+                continue
+            }
+
+            val = tokens[i]
+            const num1 = nums.pop()
+            const num2 = nums.pop()
+            if (val === '+') {
+                nums.push(num2 + num1)
+            } else if (val === '-') {
+                nums.push(num2 - num1)
+            } else if (val === '/') {
+                nums.push(parseInt(num2 / num1))
+            } else if (val === '*') {
+                nums.push(num2 * num1)
+            }
+
+        }
+
+        return nums.pop()
+    }
 }
