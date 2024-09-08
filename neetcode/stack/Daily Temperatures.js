acade28398f91a4704d9acc3891d9b02603ff40b
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-09-08 13:50:49 -0400

    completed solution

--- neetcode/stack/Daily Temperatures.js
+++ neetcode/stack/Daily Temperatures.js
@@ -3,5 +3,21 @@ class Solution {
      * @param {number[]} temperatures
      * @return {number[]}
      */
-    dailyTemperatures(temperatures) {}
+    dailyTemperatures(temperatures) {
+        const daily = []
+
+        for (let i = 0; i < temperatures.length; i++) {
+            let days = 0
+            let p = i + 1
+            while(p < temperatures.length) {
+                if (temperatures[p] > temperatures[i]) {
+                    days = p - i
+                    break
+                }
+                p ++;
+            }
+            daily.push(days)
+        }
+        return daily
+    }
 }
