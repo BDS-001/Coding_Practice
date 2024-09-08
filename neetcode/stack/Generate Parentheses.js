2e33c0232cbbb0d26bea140be8738b714a021299
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-09-08 13:40:39 -0400

    completed soltion

--- neetcode/stack/Generate Parentheses.js
+++ neetcode/stack/Generate Parentheses.js
@@ -3,5 +3,21 @@ class Solution {
      * @param {number} n
      * @return {string[]}
      */
-    generateParenthesis(n) {}
+    generateParenthesis(n) {
+        const res = []
+
+        const genPar = (set = '', open = 0) => {
+            if (set.length > n * 2) return
+            if (set.length === n * 2 && open === 0) res.push(set)
+
+            if (open > 0) genPar(set + ')', open - 1)
+            genPar(set + '(', open + 1)
+        }
+        
+        genPar()
+        return res
+    }
 }
+
+
+console.log(new Solution().generateParenthesis(1))
