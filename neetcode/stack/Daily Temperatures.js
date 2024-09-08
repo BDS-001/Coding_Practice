01a566f9707a6c70caeb93303e16dee357bdc850
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-09-08 14:10:07 -0400

    second optimized solution

--- neetcode/stack/Daily Temperatures.js
+++ neetcode/stack/Daily Temperatures.js
@@ -1,23 +1,47 @@
+// class Solution {
+//     /**
+//      * @param {number[]} temperatures
+//      * @return {number[]}
+//      */
+//     dailyTemperatures(temperatures) {
+//         const daily = []
+
+//         for (let i = 0; i < temperatures.length; i++) {
+//             let days = 0
+//             let p = i + 1
+//             while(p < temperatures.length) {
+//                 if (temperatures[p] > temperatures[i]) {
+//                     days = p - i
+//                     break
+//                 }
+//                 p ++;
+//             }
+//             daily.push(days)
+//         }
+//         return daily
+//     }
+// }
+
 class Solution {
     /**
      * @param {number[]} temperatures
      * @return {number[]}
      */
     dailyTemperatures(temperatures) {
-        const daily = []
+        const daily = new Array(temperatures.length).fill(0)
+        const tempStack = []
+        let i = 0
 
-        for (let i = 0; i < temperatures.length; i++) {
-            let days = 0
-            let p = i + 1
-            while(p < temperatures.length) {
-                if (temperatures[p] > temperatures[i]) {
-                    days = p - i
-                    break
-                }
-                p ++;
+        while (i < temperatures.length) {
+            if (tempStack.length > 0 && temperatures[i] > tempStack[tempStack.length - 1].temp) {
+                const temp = tempStack.pop()
+                daily[temp.index] = i - temp.index
+            } else {
+                tempStack.push({temp: temperatures[i], index: i})
+                i ++;
             }
-            daily.push(days)
         }
         return daily
     }
 }
+
