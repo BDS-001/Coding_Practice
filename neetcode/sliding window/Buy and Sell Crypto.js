dd609af367a49458c0ea165a0c73f0c24363a68f
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-09-09 19:45:07 -0400

    completed solution

--- neetcode/sliding window/Buy and Sell Crypto.js
+++ neetcode/sliding window/Buy and Sell Crypto.js
@@ -3,5 +3,20 @@ class Solution {
      * @param {number[]} prices
      * @return {number}
      */
-    maxProfit(prices) {}
+    maxProfit(prices) {
+        let max = 0
+
+        let left = 0
+        let right = left + 1
+
+        while(right < prices.length) {
+            if (prices[left] > prices[right]) {
+                left = right
+            } else {
+                max = Math.max(max, prices[right] - prices[left])
+            }
+            right ++;
+        }
+        return max
+    }
 }
