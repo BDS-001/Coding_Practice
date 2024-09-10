212b46e211a1dde89c92cdca4da08b3568d98e73
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-09-09 20:31:24 -0400

    completed solution

--- neetcode/sliding window/Longest Substring Without Duplicates.js
+++ neetcode/sliding window/Longest Substring Without Duplicates.js
@@ -3,5 +3,23 @@ class Solution {
      * @param {string} s
      * @return {number}
      */
-    lengthOfLongestSubstring(s) {}
+    lengthOfLongestSubstring(s) {
+        let longest = 0
+        let string = []
+
+        let right = 0
+        while (right < s.length) {
+            if (string.includes(s[right])) {
+                //if char in substring remove everything untill the repeated char
+                longest = Math.max(string.length, longest)
+                while(string[0] != s[right]) {
+                    string.shift()
+                }
+                string.shift()
+            }
+            string.push(s[right])
+            right ++;
+        }
+        return Math.max(longest, string.length)
+    }
 }
