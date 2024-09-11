9ef31b44e90e3ca375d1abb6314a6d08a68ab781
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-09-11 19:46:53 -0400

    solution in progress

--- neetcode/stack/Largest-Rectangle-In-Histogram.js
+++ neetcode/stack/Largest-Rectangle-In-Histogram.js
@@ -3,5 +3,19 @@ class Solution {
      * @param {number[]} heights
      * @return {number}
      */
-    largestRectangleArea(heights) {}
+    largestRectangleArea(heights) {
+        let stack = []
+        let max = null
+        for (let i = 0; i < heights.length; i++) {
+            while(stack.length > 0 || stack.height > heights[i]) {
+                const current = stack.shift()
+                max = Math.max(max, current.height * (i - current.i))
+            }
+            stack.unshift({height: heights[i], i: i})
+            
+        }
+        
+        console.log(stack[0].height * (stack[0].i - stack[stack.length - 1].i + 1), stack[0].height, stack[0].i, stack)
+        return stack.length === 0 ? max : Math.max(max, stack[0].height * (stack[0].i - stack[stack.length - 1].i + 1))
+    }
 }
