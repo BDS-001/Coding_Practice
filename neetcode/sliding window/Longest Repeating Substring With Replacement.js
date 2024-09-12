0a189b26f3375495953cea22b7251e15ddaa402c
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-09-11 21:10:43 -0400

    solution in progress

--- neetcode/sliding window/Longest Repeating Substring With Replacement.js
+++ neetcode/sliding window/Longest Repeating Substring With Replacement.js
@@ -4,5 +4,26 @@ class Solution {
      * @param {number} k
      * @return {number}
      */
-    characterReplacement(s, k) {}
+    characterReplacement(s, k) {
+        const groups = []
+        let right = 0
+        let left = 0
+        while(right < s.length) {
+            if (s[right] != s[left]) {
+                groups.push({letter: s[left], pointers:[left, right - 1], length: right - left})
+                left = right
+                continue
+            }
+            if (right === s.length - 1) {
+                console.log(right, s[left], left, right)
+                groups.push({letter: s[left], pointers:[left, right], length: right + 1 - left})
+                break
+            }
+            right ++
+        }
+        let longest = 0
+        console.log(groups)
+
+        return longest
+    }
 }
