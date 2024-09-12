de9405e5c9fb50c28a04b1ccca354bd9f29e8990
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-09-12 12:26:06 -0400

    completed

--- neetcode/sliding window/Longest-Repeating-Substring-With-Replacement.js
+++ neetcode/sliding window/Longest-Repeating-Substring-With-Replacement.js
@@ -12,8 +12,20 @@ class Solution {
         let left = 0
         let longest = 0
 
-        while(right < s.length) {
-            freq.set(s[right], 1 + (count.get(s[right]) || 0))
+        while(right <= s.length) {
+            const vals = [...freq.values()]
+            const total = vals.length > 0 ? vals.reduce((total, cur) => total += cur) : 0
+            const valid = total === 0 ? true : total - Math.max(...vals) <= k
+            console.log('Map: ', vals, '\ntotal:',  total, '\nlongest', longest, '\nk: ',  k, '\nvalid: ', valid, '\n', '---------------------------------------')
+
+            if (valid) {
+                right < s.length ? freq.set(s[right], 1 + (freq.get(s[right]) || 0)) : null
+                right ++
+                longest = Math.max(total, longest)
+                continue
+            }
+            freq.set(s[left], freq.get(s[left]) - 1)
+            left ++
         }
 
 
