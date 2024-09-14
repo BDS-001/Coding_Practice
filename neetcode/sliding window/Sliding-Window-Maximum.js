0cbe872d7b1daa647f9d72a9d1e113a9cbb24d23
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-09-14 13:54:42 -0400

    completed solution

--- neetcode/sliding window/Sliding-Window-Maximum.js
+++ neetcode/sliding window/Sliding-Window-Maximum.js
@@ -4,5 +4,20 @@ class Solution {
      * @param {number} k
      * @return {number[]}
      */
-    maxSlidingWindow(nums, k) {}
+    maxSlidingWindow(nums, k) {
+        const maxList = []
+        let left = 0
+        let right = k - 1
+
+        while(right < nums.length) {
+            let max = -Infinity
+            for (let i = left; i <= right; i++) {
+                max = Math.max(nums[i], max)
+            }
+            maxList.push(max)
+            left ++;
+            right ++;
+        }
+        return maxList
+    }
 }
