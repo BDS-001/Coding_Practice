55f8753983a879131de983487d77183e4cd40180
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-09-17 17:33:03 -0400

    solution in progress

--- neetcode/linked-list/Reorder-Linked-List.js
+++ neetcode/linked-list/Reorder-Linked-List.js
@@ -13,5 +13,37 @@ class Solution {
      * @param {ListNode} head
      * @return {void}
      */
-    reorderList(head) {}
+    reorderList(head) {
+        //get the length and find the stopping point
+        let length = 0
+        let p = head
+        while(p) {
+            length ++;
+            p = p.next
+        }
+        const half = Math.ceil(length / 2)
+        let startP = head
+        let startAhead = startP?.next
+
+        console.log('here')
+        for (let i = half; i > 0; i--) {
+            let curr = startP
+            let prev = null
+            
+            while(curr.next) {
+                prev = curr
+                curr = curr.next
+            }
+            console.log(curr)
+
+            startP.next = curr
+            curr.next = startAhead
+            if (i != 1) prev.next = null
+
+            startP = startAhead
+            startAhead = startAhead?.next
+        }
+        
+        return head
+    }
 }
