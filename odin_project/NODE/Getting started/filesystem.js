f9f24759126597e5b03666f6715e75939cfeb5b4
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-09-18 18:03:02 -0400

    reading file

--- odin_project/NODE/Getting started/filesystem.js
+++ odin_project/NODE/Getting started/filesystem.js
@@ -5,21 +5,37 @@ const content = 'testing node js filesystem'
 const filepath = path.join(__dirname, 'fsTest.txt')
 console.log(__dirname)
 
-//sync
-try {
-    fs.writeFileSync(filepath, content)
-    console.log('file was written successfully')
-} catch (err) {
-    console.log(err)
+async function generateFile() {
+    console.log('\nWritting File:\n-----------------------------------------')
+    //write
+    try {
+        fs.writeFileSync(filepath, content)
+        console.log('file was written successfully')
+    } catch (err) {
+        console.log(err)
+    }
+
+    //append
+    const newContent = '\nThis should append to the file isntead of overwrite it'
+    try {
+        fs.appendFileSync(filepath, newContent)
+        console.log('file was appended successfully')
+    } catch (err) {
+        console.log(err)
+    }
 }
 
-//append and async
-const newContent = '\nThis should append to the file isntead of overwrite it'
-fs.appendFile(filepath, newContent, {flag: 'a+',}, err => {
-    if (err) {
-        console.log(err)
-    } else {
-        console.log('content was appended successfully')
-    }
-})
+async function readFile() {
+    await generateFile()
+    console.log('\nReading File:\n-----------------------------------------')
 
+    fs.readFile(filepath, 'utf8', (err, data) => {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log(data)
+        }
+    })
+}
+
+readFile()
