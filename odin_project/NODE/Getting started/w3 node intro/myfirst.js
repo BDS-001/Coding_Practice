e7e5d59b11f44b3993cc4f0b65e27a36b87c5360
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-09-18 20:13:11 -0400

    practice node

--- odin_project/NODE/Getting started/w3 node intro/myfirst.js
+++
@@ -1,8 +0,0 @@
-const https = require('https')
-const dt = require('./myfirstmodule.js')
-
-https.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('The date and time are currently: ', dt.myDateTime())
-    res.end()
-}).listen(8080);
