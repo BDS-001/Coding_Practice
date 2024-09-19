8d06274119d6d78d54f71b415a40f6111c063805
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-09-19 12:11:13 -0400

    practice file server with node

---
+++ odin_project/NODE/w3-node-intro/file_server_demo/file_server.js
@@ -0,0 +1,17 @@
+const url = require('url')
+const fs = require('fs')
+const http = require('http')
+
+http.createServer(function (req, res) {
+    const q = url.parse(req.url, true)
+    const filename = '.' + q.pathname
+    fs.reafFile(filename, function (err, data) {
+        if (err) {
+            res.writeHead(404, {'Content-Type': 'text/html'})
+            return res.end('404 Not Found')
+        }
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        res.write(data)
+        return res.end()
+    })
+}).listen(8080)
