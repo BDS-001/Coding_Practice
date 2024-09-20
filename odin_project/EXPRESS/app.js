69013b0cc4c5e920e117473b9552f4d6f352dc7f
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-09-20 17:19:58 -0400

    added backup Port

--- odin_project/EXPRESS/app.js
+++ odin_project/EXPRESS/app.js
@@ -4,5 +4,5 @@ const app = express();
 
 app.get("/", (req, res) => res.send("Hello, world!"));
 
-const PORT = parseInt(process.env.USE_PORT, 10);
+const PORT = parseInt(process.env.USE_PORT, 10) || 3000;
 app.listen(PORT, () => console.log(`My first Express app - listening on port ${PORT}!`));
