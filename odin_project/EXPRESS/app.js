43a82c9d35b4bb940a61e30b8cac90b9bbcf162b
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-09-20 17:37:59 -0400

    added more routing

--- odin_project/EXPRESS/app.js
+++ odin_project/EXPRESS/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => res.send("Hello, world!"));
+app.get("/about", (req, res) => res.send("This is an app to test out express with node!"));
 
 const PORT = parseInt(process.env.USE_PORT, 10) || 3000;
 app.listen(PORT, () => console.log(`My first Express app - listening on port ${PORT}!`));
