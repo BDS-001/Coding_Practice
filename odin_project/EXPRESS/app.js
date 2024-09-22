f6ab6e3dea6c943f1ee7cedd920a5605db677c0d
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-09-22 19:02:25 -0400

    used app.all()

--- odin_project/EXPRESS/app.js
+++ odin_project/EXPRESS/app.js
@@ -5,5 +5,12 @@ const app = express();
 app.get("/", (req, res) => res.send("Hello, world!"));
 app.get("/about", (req, res) => res.send("This is an app to test out express with node!"));
 
+//app.all() used for all requests not just get
+//'*' used to match everything
+app.all('*', (req, res) => {
+    res.send(`Got a ${req.method} request at /api`);
+  });
+
+
 const PORT = parseInt(process.env.USE_PORT, 10) || 3000;
 app.listen(PORT, () => console.log(`My first Express app - listening on port ${PORT}!`));
