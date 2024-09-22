9789a43c2003af4039edbda9b564be4f36760bf2
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-09-22 19:22:10 -0400

    practice queries

--- odin_project/EXPRESS/app.js
+++ odin_project/EXPRESS/app.js
@@ -5,11 +5,19 @@ const app = express();
 app.get("/", (req, res) => res.send("Hello, world!"));
 app.get("/about", (req, res) => res.send("This is an app to test out express with node!"));
 app.get('/:user/messages', (req, res) => {
+    //exmple url: http://localhost:3000/joe/messages?this=that&foo=bar
     console.log(req.params)
-    res.send(`${req.params.user}'s list of messages`)
+    console.log(req.query)
+    
+    const userMessages = `${req.params.user}'s list of messages`;
+    const queryList = `list of queries: ${JSON.stringify(req.query)}`;
+    
+    res.send(`${userMessages}\n${queryList}`);
 })
+
 //app.all() used for all requests not just get
 //'*' used to match everything
+//catch all, needs to be at the end of the code
 app.all('*', (req, res) => {
     res.send(`Got a ${req.method} request at /api`);
   });
