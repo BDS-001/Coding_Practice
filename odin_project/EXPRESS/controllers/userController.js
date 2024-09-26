04d95d932d6f80fde675b4306c7b7e347fa7327a
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-09-26 12:46:37 -0400

    setup funciton to use async handler

--- odin_project/EXPRESS/controllers/userController.js
+++ odin_project/EXPRESS/controllers/userController.js
@@ -1,30 +1,22 @@
-// user controller file - controllers/userController.js
+const asyncHandler = require('express-async-handler')
 
 async function someDBQueryToGetUser(userId) {
     return {id: userId, name: 'bob'}
 }
 
-const getUserById = async (req, res) => {
+// Any errors that is thrown in this function will automatically be caught and call the `next` function
+const getUserById = asyncHandler(async (req, res) => {
     const userId = req.params.id;
   
-    try {
-      const user = await someDBQueryToGetUser(userId);
+    const user = await someDBQueryToGetUser(userId);
   
-      if (!user) {
-        res.status(404).send("User not found");
-        return;
-      }
-  
-      res.send(`User found: ${user.name}`);
-    } catch (error) {
-      console.error("Error retrieving user:", error);
-      res.status(500).send("Internal Server Error");
-  
-      // or we can call next(error) instead of sending a response here
-      // Using `next(error)` however will only render an error page in the express' default view and respond with the whole html to the client.
-      // So we will need to create a special type of middleware function if we want a different response.
+    if (!user) {
+      res.status(404).send("User not found");
+      return;
     }
-  };
+  
+    res.send(`User found: ${user.name}`);
+  });
 
   module.exports = {
     getUserById
