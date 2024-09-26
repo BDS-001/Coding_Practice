afc841ab4cf82a2605fec4b8b4c0c3f063a5d39d
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-09-26 00:08:30 -0400

    floyds cycle algorithm solution

--- neetcode/linked-list/Find_Duplicate_Integer.js
+++ neetcode/linked-list/Find_Duplicate_Integer.js
@@ -1,14 +1,36 @@
+// class Solution {
+//     /**
+//      * @param {number[]} nums
+//      * @return {number}
+//      */
+//     findDuplicate(nums) {
+//         const used = new Set()
+//         for (let i = 0; i < nums.length; i++) {
+//             if (used.has(nums[i])) return nums[i]
+//             used.add(nums[i])
+//         }
+//     }
+// }
+
 class Solution {
     /**
      * @param {number[]} nums
      * @return {number}
      */
     findDuplicate(nums) {
-        const used = new Set()
-        for (let i = 0; i < nums.length; i++) {
-            if (used.has(nums[i])) return nums[i]
-            used.add(nums[i])
+        let slow = 0
+        let fast = 0
+        while(true) {
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if (slow === fast) break
+        }
+
+        let slow2 = 0
+        while (true) {
+            slow = nums[slow]
+            slow2 = nums [slow2]
+            if (slow === slow2) return slow
         }
     }
 }
-
