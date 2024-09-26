a3a25b487df02043c5c1338f1976cecf8d529a1c
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-09-25 23:39:34 -0400

    solution complete

--- neetcode/linked-list/Find_Duplicate_Integer.js
+++ neetcode/linked-list/Find_Duplicate_Integer.js
@@ -3,5 +3,12 @@ class Solution {
      * @param {number[]} nums
      * @return {number}
      */
-    findDuplicate(nums) {}
+    findDuplicate(nums) {
+        const used = new Set()
+        for (let i = 0; i < nums.length; i++) {
+            if (used.has(nums[i])) return nums[i]
+            used.add(nums[i])
+        }
+    }
 }
+
