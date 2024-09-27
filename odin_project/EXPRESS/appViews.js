143b61e791454d36711d309be975b2cbeda69e05
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-09-27 14:06:39 -0400

    removed old root router

--- odin_project/EXPRESS/appViews.js
+++ odin_project/EXPRESS/appViews.js
@@ -6,10 +6,6 @@ const path = require("node:path");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-    res.render("index", { message: "EJS rocks!" });
-});  
-
 const links = [
     { href: "/", text: "Home" },
     { href: "about", text: "About" },
