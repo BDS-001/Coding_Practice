3c7d9c85bd4f8c5df9580530c0d670da474858f6
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-10-01 12:20:41 -0400

    solution in progress

--- neetcode/Binary_Search/Search_2D_Matrix.js
+++ neetcode/Binary_Search/Search_2D_Matrix.js
@@ -5,33 +5,42 @@ class Solution {
      * @return {boolean}
      */
     searchMatrix(matrix, target) {
-        let top = 0
-        let bottom = matrix.length - 1
-        let row = null
+        if (matrix.length === 0 || matrix[0].length === 0) return false;
 
-        while(top <= bottom) {
-            const mid = Math.floor( (top + bottom) / 2)
-            if (top === bottom || matrix[top][0] === target) {
-                row = matrix[mid]
-                break
-            } else if (matrix[top][0] > target) {
-                bottom = mid - 1
-            } else {
-                top = mid + 1
+        let top = 0;
+        let bottom = matrix.length - 1;
+        let row = null;
+
+        if (matrix.length === 1) {
+            row = matrix[0];
+        } else {
+            while (top <= bottom) {
+                const mid = Math.floor((top + bottom) / 2);
+                if (matrix[mid][0] <= target && (mid === matrix.length - 1 || matrix[mid + 1][0] > target)) {
+                    row = matrix[mid];
+                    break;
+                } else if (matrix[mid][0] > target) {
+                    bottom = mid - 1;
+                } else {
+                    top = mid + 1;
+                }
             }
         }
 
+        if (row === null) return false;
+
         let start = 0
         let end = matrix.length - 1
+        console.log(row)
         while (start <= end) {
             const center = Math.floor( (start + end) / 2)
-            if (row[center] === target) return center
+            if (row[center] === target) return true
             if (row[center] > target) {
                 end = center - 1
             } else {
                 start = center + 1
             }
         }
-        return -1
+        return false
     }
 }
