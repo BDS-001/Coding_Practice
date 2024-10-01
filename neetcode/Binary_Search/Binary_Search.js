f7b6950e74b056e6f450affeaee8f4c9265d710a
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-09-30 20:02:38 -0400

    completed solution

--- neetcode/Binary_Search/Binary_Search.js
+++ neetcode/Binary_Search/Binary_Search.js
@@ -4,5 +4,18 @@ class Solution {
      * @param {number} target
      * @return {number}
      */
-    search(nums, target) {}
+    search(nums, target) {
+        let start = 0
+        let end = nums.length - 1
+        while (start <= end) {
+            const center = Math.floor( (start + end) / 2)
+            if (nums[center] === target) return center
+            if (nums[center] > target) {
+                end = center - 1
+            } else {
+                start = center + 1
+            }
+        }
+        return -1
+    }
 }
