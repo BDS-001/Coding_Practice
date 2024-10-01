66003dd3043de57040a7ea674c868fbc73c520b7
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-09-30 20:32:10 -0400

    solution in progress

--- neetcode/Binary_Search/Search_2D_Matrix.js
+++ neetcode/Binary_Search/Search_2D_Matrix.js
@@ -4,5 +4,34 @@ class Solution {
      * @param {number} target
      * @return {boolean}
      */
-    searchMatrix(matrix, target) {}
+    searchMatrix(matrix, target) {
+        let start = 0
+        let end = matrix.length - 1
+        let nums = null
+
+        while(start <= end) {
+            const center = Math.floor( (start + end) / 2)
+            if (matrix[center] === target) {
+                nums = matrix[center]
+            }
+            if (matrix[center] > target) {
+                end = center - 1
+            } else {
+                start = center + 1
+            }
+        }
+
+        start = 0
+        end = matrix.length - 1
+        while (start <= end) {
+            const center = Math.floor( (start + end) / 2)
+            if (nums[center] === target) return center
+            if (nums[center] > target) {
+                end = center - 1
+            } else {
+                start = center + 1
+            }
+        }
+        return -1
+    }
 }
