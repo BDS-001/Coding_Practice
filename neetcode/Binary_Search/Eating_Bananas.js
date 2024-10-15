39db9e9804ebc5d88bcef1f26ad98a7aa0931f25
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-10-14 23:34:30 -0400

    solution in progress

--- neetcode/Binary_Search/Eating_Bananas.js
+++ neetcode/Binary_Search/Eating_Bananas.js
@@ -4,5 +4,24 @@ class Solution {
      * @param {number} h
      * @return {number}
      */
-    minEatingSpeed(piles, h) {}
+    minEatingSpeed(piles, h) {
+        const eatBananas = (k, h, piles) => {
+            let count = 0
+            let curr = 0
+            while(count <= h) {
+                piles[curr] -= k
+                if (piles[curr] <= 0) curr ++
+                if (curr === piles.legth) return k
+                count ++;
+            }
+            return null
+        }
+
+        let min = 1
+        let max = Math.max(piles)
+        let k = Math.floor((h+1) / 2)
+        while (min <= h) {
+
+        }
+    }
 }
