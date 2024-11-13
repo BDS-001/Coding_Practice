b26f56808470bdbbda714e61e383536428e28ed7
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-11-12 20:13:41 -0500

    initial commit

---
+++ odin_project/authentication_practice/app.js
@@ -0,0 +1,25 @@
+const path = require("node:path");
+const { Pool } = require("pg");
+const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require('passport-local').Strategy;
+require('dotenv').config()
+
+const username = process.env.DATABASE_USERNAME
+const pass = process.env.DATABASE_PASS
+const pool = new Pool({
+  connectionString: `postgresql://${username}:${pass}@localhost:5432/auth_pract`
+});
+
+const app = express();
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => res.render("index"));
+
+app.listen(3000, () => console.log("app listening on port 3000!"));
