0b2ed615d030bc02dc26fc4d2e65bddb8f82caa7
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-11-14 14:57:33 -0500

    add: app2 follows zach Gollwitzer tutorial using psql instead of mongoDB

---
+++ odin_project/authentication_practice/app2.js
@@ -0,0 +1,39 @@
+const path = require("node:path");
+const { Pool } = require("pg");
+const express = require("express");
+const session = require("express-session");
+const pgSession = require('connect-pg-simple')(session);
+const passport = require("passport");
+require('dotenv').config()
+
+const username = process.env.DATABASE_USERNAME
+const pass = process.env.DATABASE_PASS
+const pool = new Pool({
+  connectionString: `postgresql://${username}:${pass}@localhost:5432/auth_pract`
+});
+
+const app = express();
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+
+sessionStore = new pgSession({
+    pool: pool,
+    tableName: 'sesssions'
+})
+app.use(session({ 
+    secret: "some secret", 
+    resave: false, 
+    saveUninitialized: true,
+    store: sessionStorage,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24
+    }
+}));
+app.use(passport.session());
+
+app.get("/", (req, res, next) => {
+    res.send('<h1>Hello World (Sessions)</h1>')
+});
+
+const PORT = 3000
+app.listen(PORT, () => console.log(`Listening on port ${PORT}!\nVisit: http://localhost:${PORT}/`));
