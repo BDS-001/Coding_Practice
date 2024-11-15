c51a9fb2f04f8fba232438517104c94e11bb3ec0
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-11-14 23:41:31 -0500

    add: passport setup with local strategy

--- odin_project/authentication_practice/app2.js
+++ odin_project/authentication_practice/app2.js
@@ -2,6 +2,9 @@ const { Pool } = require("pg");
 const express = require("express");
 const session = require("express-session");
 const pgSession = require('connect-pg-simple')(session);
+const passport = require("passport")
+const bcrypt = require('bcryptjs')
+const LocalStrategy = require('passport-local').Strategy;
 require('dotenv').config()
 
 const username = process.env.DATABASE_USERNAME
@@ -28,6 +31,26 @@ app.use(session({
         maxAge: 1000 * 60 * 60 * 24
     }
 }));
+
+passport.use(
+    new LocalStrategy(async (username, password, done) => {
+      try {
+        const { rows } = await pool.query("SELECT * FROM users WHERE username = $1", [username]);
+        const user = rows[0];
+  
+        if (!user) {
+          return done(null, false, { message: "Incorrect username" });
+        }
+        const match = await bcrypt.compare(password, user.password);
+        if (!match) {
+          return done(null, false, { message: "Incorrect password" })
+        }
+        return done(null, user);
+      } catch(err) {
+        return done(err);
+      }
+    })
+  );
 
 app.get("/", (req, res, next) => {
     (req.session.viewCount) ? req.session.viewCount++ : req.session.viewCount = 1
