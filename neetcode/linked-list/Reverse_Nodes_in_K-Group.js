891786818abf88301ea89f786c464a99a118ebb0
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-11-18 17:31:19 -0500

    add: starter code

---
+++ neetcode/linked-list/Reverse_Nodes_in_K-Group.js
@@ -0,0 +1,18 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @param {number} k
+     * @return {ListNode}
+     */
+    reverseKGroup(head, k) {}
+}
