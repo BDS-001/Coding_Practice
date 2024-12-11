0a56851c3a8f83e247b8de8209a84f79d37fcb56
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-12-10 22:18:51 -0500

    update: completed solution

--- neetcode/Trees/Invert_Binary_Tree.js
+++ neetcode/Trees/Invert_Binary_Tree.js
@@ -14,5 +14,21 @@ class Solution {
      * @param {TreeNode} root
      * @return {TreeNode}
      */
-    invertTree(root) {}
+    invertTree(root) {
+        if (!root) return root
+        const queue = [root]
+
+        while(queue.length > 0) {
+            const currentNode = queue.pop()
+            const left = currentNode.left
+            const right = currentNode.right
+
+            currentNode.left = right
+            currentNode.right = left
+
+            if (left) queue.push(left)
+            if (right) queue.push(right)
+        }
+    return root
+    }
 }
