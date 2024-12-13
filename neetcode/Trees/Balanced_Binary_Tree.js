26154bcb9a996ea81e231f842df273c495ec64f6
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-12-12 19:16:04 -0500

    update: completed solution

--- neetcode/Trees/Balanced_Binary_Tree.js
+++ neetcode/Trees/Balanced_Binary_Tree.js
@@ -14,5 +14,16 @@ class Solution {
      * @param {TreeNode} root
      * @return {boolean}
      */
-    isBalanced(root) {}
+    isBalanced(root) {
+        if (!root) return true
+        const leftLength = this.nodeLength(root.left)
+        const rightLength = this.nodeLength(root.right)
+        if (Math.abs(leftLength - rightLength) > 1) return false
+        return true && this.isBalanced(root.left) && this.isBalanced(root.right)
+    }
+
+    nodeLength(node) {
+        if (!node) return 0
+        return 1 + Math.max(this.nodeLength(node.left), this.nodeLength(node.right))
+    }
 }
