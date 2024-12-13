61b4b8e5de35761851464998d27ce6622e4cddcb
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-12-12 19:01:26 -0500

    update: completed solution

--- neetcode/Trees/Diameter_of_Binary_Tree.js
+++ neetcode/Trees/Diameter_of_Binary_Tree.js
@@ -14,5 +14,18 @@ class Solution {
      * @param {TreeNode} root
      * @return {number}
      */
-    diameterOfBinaryTree(root) {}
+    maxLength(root) {
+        if (!root) return 0
+        return 1 + Math.max(this.maxLength(root.left), this.maxLength(root.right))
+    }
+
+    diameterOfBinaryTree(root) {
+        if (!root) return 0
+
+        const leftMax = this.maxLength(root.left)
+        const rightMax = this.maxLength(root.right)
+        const rootDiameter = leftMax + rightMax
+        const sub = Math.max(this.diameterOfBinaryTree(root.left), this.diameterOfBinaryTree(root.right))
+        return Math.max(rootDiameter, sub)
+    }
 }
