362c70abf79a95273dfa60cab8fb6153c91d813d
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-12-13 19:33:28 -0500

    update: completed solution

--- neetcode/Trees/Subtree_of_Another_Tree.js
+++ neetcode/Trees/Subtree_of_Another_Tree.js
@@ -15,5 +15,27 @@ class Solution {
      * @param {TreeNode} subRoot
      * @return {boolean}
      */
-    isSubtree(root, subRoot) {}
+    isSubtree(root, subRoot) {
+        if (!root && !subRoot) return true
+        if (!root || !subRoot) return false
+
+        const stack = [root]
+        while(stack.length > 0) {
+            const node = stack.shift()
+            if (!node) continue
+
+            if (node.val === subRoot.val) {
+                if (this.matchingTrees(node, subRoot)) return true
+            }
+            stack.push(node.left)
+            stack.push(node.right)
+        }
+        return false
+    }
+
+    matchingTrees(root, subRoot) {
+        if (!root && !subRoot) return true
+        if (!root || !subRoot) return false
+        return root.val === subRoot.val ? true && this.matchingTrees(root.left, subRoot.left) && this.matchingTrees(root.right, subRoot.right) : false
+    }
 }
