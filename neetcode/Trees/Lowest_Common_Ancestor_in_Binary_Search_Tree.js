da2e1baaf86c8435209c764b0b54154ef8726dd5
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-12-15 20:12:03 -0500

    update: first attempt

--- neetcode/Trees/Lowest_Common_Ancestor_in_Binary_Search_Tree.js
+++ neetcode/Trees/Lowest_Common_Ancestor_in_Binary_Search_Tree.js
@@ -16,5 +16,26 @@ class Solution {
      * @param {TreeNode} q
      * @return {TreeNode}
      */
-    lowestCommonAncestor(root, p, q) {}
+    lowestCommonAncestor(root, p, q) {
+        if (!root) return
+        let left = false
+        let right = false
+        let stack = [root]
+        while(!(left && right) && stack.length > 0) {
+            const node = stack.shift()
+            const existsLeft = this.checkExists(node.left, p.val, q.val)
+            const existsRight = this.checkExists(node.right, p.val, q.val)
+
+            if (existsLeft && existsRight) return node
+            if (existsLeft) stack.push(root.left)
+            if (existsRight) stack.push(root.right)
+        }
+
+    }
+
+    checkExists(root, p, q) {
+        if (!root) return false
+        if (root.val === p || root.val === q) return true
+        return false || this.checkExists(root.left, p, q) || this.checkExists(root.right, p, q)
+    }
 }
