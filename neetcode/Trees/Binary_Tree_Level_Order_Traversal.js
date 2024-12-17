fd7226db747e54355f92a0fc85a03a709a142506
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-12-16 22:43:08 -0500

    update: current solution, need to change returned obects into integers

--- neetcode/Trees/Binary_Tree_Level_Order_Traversal.js
+++ neetcode/Trees/Binary_Tree_Level_Order_Traversal.js
@@ -14,5 +14,22 @@ class Solution {
      * @param {TreeNode} root
      * @return {number[][]}
      */
-    levelOrder(root) {}
+    levelOrder(root) {
+        const stack = [[root]]
+        let prev = -1
+        let current = stack.length - 1
+        while(prev !== current) {
+            prev = current
+            const level = []
+            const currLevel = stack[current]
+            for (let i = 0; i < currLevel.length; i++) {
+                const node = currLevel[i]
+                if (node.left) level.push(node.left)
+                if (node.right) level.push(node.right)
+            }
+            if(level.length > 0) stack.push(level)
+            current = stack.length - 1
+            
+        }
+    }
 }
