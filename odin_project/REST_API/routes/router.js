d0b16cfa7a0e2ddaf859ad3813eedb796c18f899
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-12-18 22:08:20 -0500

    add: rout handler for message POST

--- odin_project/REST_API/routes/router.js
+++ odin_project/REST_API/routes/router.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const router = Router();
+import { v4 as uuidv4 } from 'uuid';
 
 let users = {
     1: {
@@ -25,32 +26,48 @@ let users = {
     },
   };
 
+
+//users
 router.get('/users', (req, res) => {
-    return res.send(Object.values(users));
+  return res.send(Object.values(users));
 });
 
 router.get('/users/:userId', (req, res) => {
-    return res.send(users[req.params.userId]);
+  return res.send(users[req.params.userId]);
 });
 
-router.get('/messages', (req, res) => {
-    return res.send(Object.values(messages));
-});
-  
-router.get('/messages/:messageId', (req, res) => {
-    return res.send(messages[req.params.messageId]);
-});
-  
 router.post('/users', (req, res) => {
-return res.send('POST HTTP method on user resource\n');
+  return res.send('POST HTTP method on user resource\n');
 });
-
+  
 router.put('/users/:userId', (req, res) => {
-return res.send(`PUT HTTP method on user/${req.params.userId} resource\n`);
+  return res.send(`PUT HTTP method on user/${req.params.userId} resource\n`);
+});
+  
+router.delete('/users/:userId', (req, res) => {
+  return res.send(`DELETE HTTP method on user/${req.params.userId} resource\n`);
 });
 
-router.delete('/users/:userId', (req, res) => {
-return res.send(`DELETE HTTP method on user/${req.params.userId} resource\n`);
+
+//messages
+router.get('/messages', (req, res) => {
+  return res.send(Object.values(messages));
+});
+
+router.get('/messages/:messageId', (req, res) => {
+  return res.send(messages[req.params.messageId]);
+});
+
+router.post('/messages', (req, res) => {
+  const id = uuidv4();
+  const message = {
+    id,
+    text: req.body.text
+  };
+
+  messages[id] = message;
+
+  return res.send(message);
 });
 
 module.exports = router;
