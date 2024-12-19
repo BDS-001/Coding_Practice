f95c527cea22e4c2285d0c1279d77b6feac2215d
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-12-18 21:38:40 -0500

    update: router routes are more RESTful

--- odin_project/REST_API/routes/router.js
+++ odin_project/REST_API/routes/router.js
@@ -1,20 +1,20 @@
 const { Router } = require("express");
 const router = Router();
 
-router.get('/', (req, res) => {
-  return res.send('Received a GET HTTP method');
+router.get('/users', (req, res) => {
+    return res.send('GET HTTP method on user resource\n');
+});
+  
+router.post('/users', (req, res) => {
+return res.send('POST HTTP method on user resource\n');
 });
 
-router.post('/', (req, res) => {
-  return res.send('Received a POST HTTP method');
+router.put('/users/:userId', (req, res) => {
+return res.send(`PUT HTTP method on user/${req.params.userId} resource\n`);
 });
 
-router.put('/', (req, res) => {
-  return res.send('Received a PUT HTTP method');
-});
-
-router.delete('/', (req, res) => {
-  return res.send('Received a DELETE HTTP method');
+router.delete('/users/:userId', (req, res) => {
+return res.send(`DELETE HTTP method on user/${req.params.userId} resource\n`);
 });
 
 module.exports = router;
