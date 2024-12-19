4ac5cec5d90cb951e14d04d08f174b34d5c61544
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-12-18 21:11:37 -0500

    add: basic express setup

---
+++ odin_project/REST_API/app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+require('dotenv').config()
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello World!\n');
+  });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}!\nVisit: http://localhost:${PORT}/`));
