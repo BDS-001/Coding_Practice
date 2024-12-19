cf35f5858c7ff7c2cf4e2d14548d5c92d4e71ea2
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-12-18 22:10:07 -0500

    add: body parser middleware

--- odin_project/REST_API/app.js
+++ odin_project/REST_API/app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 require('dotenv').config()
 const router = require('./routes/router')
-
 const app = express()
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/", router);
 
 const PORT = process.env.PORT || 3000;
