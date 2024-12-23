45548cdb6de07c40d3dcad3e4b2d91a386e1715f
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2024-12-23 00:04:58 -0500

    update: completed solution

--- neetcode/Trees/Binary_Tree_Right_Side_View.js
+++ neetcode/Trees/Binary_Tree_Right_Side_View.js
@@ -14,5 +14,22 @@ class Solution {
      * @param {TreeNode} root
      * @return {number[]}
      */
-    rightSideView(root) {}
+    rightSideView(root) {
+        if (!root) return []
+        const q = [root]
+        const res = []
+
+        let qLength = q.length
+        while(q.length > 0) {
+            for (let i = 1; i <= qLength; i++) {
+                const node = q.shift();
+                if (i === qLength) res.push(node.val)
+                
+                if (node.left) q.push(node.left)
+                if (node.right) q.push(node.right)
+            }
+        qLength = q.length
+        }
+        return res
+    }
 }
