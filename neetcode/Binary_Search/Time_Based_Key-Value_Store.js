8b551b0000dd3328205deba1cbd5b88edfbc81d5
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-01-05 20:25:11 -0500

    initial commit

---
+++ neetcode/Binary_Search/Time_Based_Key-Value_Store.js
@@ -0,0 +1,20 @@
+class TimeMap {
+    constructor() {
+        this.keyStore = new Map();
+    }
+
+    /**
+     * @param {string} key
+     * @param {string} value
+     * @param {number} timestamp
+     * @return {void}
+     */
+    set(key, value, timestamp) {}
+
+    /**
+     * @param {string} key
+     * @param {number} timestamp
+     * @return {string}
+     */
+    get(key, timestamp) {}
+}
