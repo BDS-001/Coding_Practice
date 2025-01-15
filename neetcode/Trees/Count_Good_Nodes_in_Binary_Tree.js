5aab38256f417050c5cf58d1ef54a127c97a44f1
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-01-15 00:01:07 -0500

    completed solution

--- neetcode/Trees/Count_Good_Nodes_in_Binary_Tree.js
+++ neetcode/Trees/Count_Good_Nodes_in_Binary_Tree.js
@@ -14,5 +14,18 @@ class Solution {
      * @param {TreeNode} root
      * @return {number}
      */
-    goodNodes(root) {}
+    goodNodes(root) {
+        if (root) {
+            return this.goodNodeCount(root, -Infinity)
+        }
+        return 0
+    }
+
+    goodNodeCount(node, max) {
+        let [good, left, right] = [0,0,0]
+        if (node.val >= max) good = 1
+        if (node.right) right = this.goodNodeCount(node.right, Math.max(max, node.val))
+        if (node.left) left = this.goodNodeCount(node.left, Math.max(max, node.val))
+        return good + left + right
+    }
 }
