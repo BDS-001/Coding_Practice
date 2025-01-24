a604f6e7ede8facc46cf1f65b6e130428ac70a0d
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-01-24 18:17:45 -0500

    complete: solution

--- neetcode/Trees/Binary_Tree_Maximum_Path_Sum.js
+++ neetcode/Trees/Binary_Tree_Maximum_Path_Sum.js
@@ -14,5 +14,25 @@ class Solution {
      * @param {TreeNode} root
      * @return {number}
      */
-    maxPathSum(root) {}
+
+    constructor() {
+        this.result = -Infinity
+    }
+
+    maxPathSum(root) {
+        this.getMaxSum(root)
+        return this.result
+    }
+
+    getMaxSum(node) {
+        if (!node) return 0
+
+        const left = this.getMaxSum(node.left)
+        const right = this.getMaxSum(node.right)
+
+        //check for just the node, node and left, node and right
+        const nodePathVal = Math.max(node.val, node.val + left, node.val + right, left + node.val + right, this.result)
+        if (nodePathVal > this.result) this.result = nodePathVal
+        return Math.max(0, node.val, node.val + left, node.val + right)
+    }
 }
