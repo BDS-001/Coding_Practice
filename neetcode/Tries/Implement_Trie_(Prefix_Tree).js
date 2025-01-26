5d26262c0ad60a97907e26c34cdb24eec704fd36
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-01-25 22:03:19 -0500

    initial commit

---
+++ neetcode/Tries/Implement_Trie_(Prefix_Tree).js
@@ -0,0 +1,21 @@
+class PrefixTree {
+    constructor() {}
+
+    /**
+     * @param {string} word
+     * @return {void}
+     */
+    insert(word) {}
+
+    /**
+     * @param {string} word
+     * @return {boolean}
+     */
+    search(word) {}
+
+    /**
+     * @param {string} prefix
+     * @return {boolean}
+     */
+    startsWith(prefix) {}
+}
