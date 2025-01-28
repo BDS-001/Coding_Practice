cf6e696db11592121dd2528c2d78affc02388dec
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-01-27 22:24:49 -0500

    update: completed solution

--- neetcode/Tries/Implement_Trie_(Prefix_Tree).js
+++ neetcode/Tries/Implement_Trie_(Prefix_Tree).js
@@ -1,21 +1,65 @@
+class Node {
+    constructor(letter, finalChar=false) {
+        this.letter = letter;
+        this.finalChar = finalChar;
+        this.next = new Array(26).fill(null);
+    }
+}
+
 class PrefixTree {
-    constructor() {}
+    constructor() {
+        this.root = new Node(null)
+    }
 
     /**
      * @param {string} word
      * @return {void}
      */
-    insert(word) {}
+    insert(word) {
+        let pointer = this.root
+        for (let i = 0; i < word.length; i++) {
+            const index = word.charCodeAt(i) - 97
+            const finalChar = i === word.length - 1
+            let letterPosition = pointer.next[index]
+            if (!letterPosition) {
+                letterPosition = new Node(word[i], finalChar)
+                pointer.next[index] = letterPosition
+            }
+
+            if (finalChar) {
+                letterPosition.finalChar = true
+                return
+            }
+            
+            pointer = letterPosition
+        }
+    }
 
     /**
      * @param {string} word
      * @return {boolean}
      */
-    search(word) {}
+    search(word) {
+        let pointer = this.root
+        for (let i = 0; i < word.length; i++) {
+            const letterPosition = pointer.next[word.charCodeAt(i) - 97]
+            if (!letterPosition) return false
+            if (i === word.length - 1) return letterPosition.finalChar
+            pointer = letterPosition
+        }
+    }
 
     /**
      * @param {string} prefix
      * @return {boolean}
      */
-    startsWith(prefix) {}
+    startsWith(prefix) {
+        let pointer = this.root
+        for (let i = 0; i < prefix.length; i++) {
+            const letterPosition = pointer.next[prefix.charCodeAt(i) - 97]
+            if (!letterPosition) return false
+            pointer = letterPosition
+        }
+        return true
+    }
 }
