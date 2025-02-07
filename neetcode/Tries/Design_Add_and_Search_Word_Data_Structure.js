14eac70b703d82a2c8eb5e637a8022fdfb8b9d55
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-02-06 20:22:05 -0500

    initial commit

---
+++ neetcode/Tries/Design_Add_and_Search_Word_Data_Structure.js
@@ -0,0 +1,15 @@
+class WordDictionary {
+    constructor() {}
+
+    /**
+     * @param {string} word
+     * @return {void}
+     */
+    addWord(word) {}
+
+    /**
+     * @param {string} word
+     * @return {boolean}
+     */
+    search(word) {}
+}
