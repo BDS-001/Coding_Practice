e1d43876ee9143cfb7b5e8f740c43793f25d58af
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-02-06 20:59:21 -0500

    update: completed solution

--- neetcode/Tries/Design_Add_and_Search_Word_Data_Structure.js
+++ neetcode/Tries/Design_Add_and_Search_Word_Data_Structure.js
@@ -1,15 +1,57 @@
+class Node {
+    constructor(letter, finalChar=false) {
+        this.letter = letter;
+        this.finalChar = finalChar;
+        this.next = new Array(26).fill(null);
+    }
+}
+
 class WordDictionary {
-    constructor() {}
+    constructor() {
+        this.root = new Node(null)
+    }
 
     /**
      * @param {string} word
      * @return {void}
      */
-    addWord(word) {}
+    addWord(word) {
+        let pointer = this.root
+        for (let i = 0; i < word.length; i++) {
+            const index = word.charCodeAt(i) - 97
+            const finalChar = i === word.length - 1
+            let letterPosition = pointer.next[index]
+            if (!letterPosition) {
+                letterPosition = new Node(word[i], finalChar)
+                pointer.next[index] = letterPosition
+            }
+
+            if (finalChar) {
+                letterPosition.finalChar = true
+                return
+            }
+            
+            pointer = letterPosition
+        }
+    }
 
     /**
      * @param {string} word
      * @return {boolean}
      */
-    search(word) {}
+    search(word, index = 0, pointer = this.root) {
+        if (word.length === index) return pointer.finalChar
+        const letter = word[index]
+        const letterIndex = word.charCodeAt(index) - 97
+        if (letter === '.') {
+            return pointer.next.some(letterNode => 
+                letterNode && this.search(word, index + 1, letterNode)
+            );
+        }
+
+        const letterNode = pointer.next[letterIndex]
+        if (letterNode) return this.search(word, index + 1, letterNode)
+        return false
+
+    }
 }
