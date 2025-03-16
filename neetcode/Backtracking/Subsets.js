52523f8c6dd78daeb332f952be191e0a66cc2d1a
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-03-15 23:33:15 -0400

    complete: solution

--- neetcode/Backtracking/Subsets.js
+++ neetcode/Backtracking/Subsets.js
@@ -3,5 +3,15 @@ class Solution {
      * @param {number[]} nums
      * @return {number[][]}
      */
-    subsets(nums) {}
+    subsets(nums) {
+        const res = []
+        const getSet = (curr, index) => {
+            res.push(curr)
+            for (let i = index; i < nums.length; i++) {
+                getSet([...curr, nums[i]], i + 1)
+            }
+        }
+        getSet([], 0);
+        return res;
+    }
 }
