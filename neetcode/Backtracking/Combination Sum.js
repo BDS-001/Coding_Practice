074b854d87beee7ad5b78c695c260cfeecc4a56a
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-03-16 19:48:55 -0400

    update: currently solution has duplicate entries

--- neetcode/Backtracking/Combination Sum.js
+++ neetcode/Backtracking/Combination Sum.js
@@ -6,6 +6,19 @@ class Solution {
      */
     combinationSum(nums, target) {
         const res = [];
-        
+        function combine(curr) {
+            let total = curr.reduce((acc, current) => acc + current, 0)
+            if (total > target) return
+            if (total === target) {
+                res.push(curr)
+                return
+            }
+            
+            for (let i = 0; i < nums.length; i++) {
+                combine([...curr, nums[i]])
+            }
+        }
+        combine([], 0)
+        return res
     }
 }
