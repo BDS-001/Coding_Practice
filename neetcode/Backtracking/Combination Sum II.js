e32d9c70c79d43bec8ebacf6d5161519f8e1ff89
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-03-17 23:49:21 -0400

    update: solutoin complete: sorted candidate array, used loop to find diff starting value for second recrsive call

--- neetcode/Backtracking/Combination Sum II.js
+++ neetcode/Backtracking/Combination Sum II.js
@@ -5,6 +5,8 @@ class Solution {
      * @return {number[][]}
      */
     combinationSum2(candidates, target) {
+        candidates = candidates.sort()
+        console.log(candidates)
         const res = [];
         function combine(curr, index, total=0) {
             if (total === target) {
@@ -13,7 +15,13 @@ class Solution {
             }
             if (total > target || index >= candidates.length) return
             combine([...curr, candidates[index]], index + 1, total + candidates[index])
-            combine([...curr], index + 1, total)
+            
+            let newIndex = index
+            while(newIndex < candidates.length) {
+                newIndex ++;
+                if (candidates[index] != candidates[newIndex]) break
+            }
+            combine([...curr], newIndex, total)
         }
         combine([], 0)
         return res
