fa375debeaf5d0f14f5bc7c478b6c8ef5d02bb0f
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-03-17 23:16:51 -0400

    update: solution passes unique numbers, fails duplicate candidates

--- neetcode/Backtracking/Combination Sum II.js
+++ neetcode/Backtracking/Combination Sum II.js
@@ -4,5 +4,18 @@ class Solution {
      * @param {number} target
      * @return {number[][]}
      */
-    combinationSum2(candidates, target) {}
+    combinationSum2(candidates, target) {
+        const res = [];
+        function combine(curr, index, total=0) {
+            if (total === target) {
+                res.push(curr)
+                return
+            }
+            if (total > target || index >= candidates.length) return
+            combine([...curr, candidates[index]], index + 1, total + candidates[index])
+            combine([...curr], index + 1, total)
+        }
+        combine([], 0)
+        return res
+    }
 }
