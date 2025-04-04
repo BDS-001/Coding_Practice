0dd1e68030e33cbf405a764acf57573a52eb8a26
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-04-03 21:36:52 -0400

    complete: permutation

--- neetcode/Backtracking/Permutations.js
+++ neetcode/Backtracking/Permutations.js
@@ -3,5 +3,25 @@ class Solution {
      * @param {number[]} nums
      * @return {number[][]}
      */
-    permute(nums) {}
+    permute(nums) {
+        const map = nums.reduce( (acc, current) => {
+            acc[current] = 0
+            return acc
+        }, {})
+        const res = []
+        const backtrack = (curr = [], backtrackMap = {...map}) => {
+            if (curr.length === nums.length) {
+                res.push(curr)
+                return
+            }
+            for (let i = 0; i < nums.length; i++) {
+                if (backtrackMap[nums[i]] > 0) continue
+                const newMap = {...backtrackMap}
+                newMap[nums[i]] = 1
+                backtrack([...curr, nums[i]], newMap)
+            }
+        }
+        backtrack()
+        return res
+    }
 }
