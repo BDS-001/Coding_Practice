133a4fb1e240946d23a023bf028ce8ddd344a376
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-04-04 22:01:52 -0400

    update: solution in progres

--- neetcode/Backtracking/Subsets II.js
+++ neetcode/Backtracking/Subsets II.js
@@ -3,5 +3,25 @@ class Solution {
      * @param {number[]} nums
      * @return {number[][]}
      */
-    subsetsWithDup(nums) {}
+    subsetsWithDup(nums) {
+        const sortedNums = nums.sort((a, b) => a - b)
+        const res = []
+        const backtrack = (curr = [], index = 0) => {
+            res.push(curr)
+            if (index >= sortedNums.length) return
+            backtrack([...curr, sortedNums[index]], index + 1)
+
+            let newIndex = index
+            while(sortedNums[index] === sortedNums[newIndex]) {
+                newIndex++;
+                if (newIndex >= sortedNums.length) break
+            }
+            if(newIndex < sortedNums.length) {
+                backtrack([...curr, sortedNums[newIndex]], newIndex + 1)
+            }
+        }
+        backtrack()
+        return res
+    }
 }
+
