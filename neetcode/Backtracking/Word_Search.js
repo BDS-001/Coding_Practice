d4a5c8f0243faa27a7b2ab23d89372b3f0444a06
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-04-30 15:34:25 -0400

    update: solution in progress

--- neetcode/Backtracking/Word_Search.js
+++ neetcode/Backtracking/Word_Search.js
@@ -7,6 +7,40 @@ class Solution {
     exist(board, word) {
         const used = new Map()
 
+        //backtrack function
+        const backtrack = (pos, curr, found = false) => {
+            const [row, col] = pos
+            used.set(`${row},${col}`, true)
+
+            //check the 4 directions, needs to be in bounds, not used, matches the next letter
+
+            //one row down
+            if (row + 1 < board.length && !used.has(`${row + 1},${col}`) && board[row+1][col] === word[curr]) {
+                found = found || backtrack([row+1, col], curr + 1)
+            }
+            used.delete(`${row + 1},${col}`)
+
+            //one row up
+            if (row - 1 >= 0 && !used.has(`${row - 1},${col}`) && board[row-1][col] === word[curr]) {
+                found = found || backtrack([row-1, col], curr + 1)
+            }
+            used.delete(`${row - 1},${col}`)
+
+            //one column forward
+            if (col + 1 < board[0].length && !used.has(`${row},${col + 1}`) && board[row][col + 1] === word[curr]) {
+                found = found || backtrack([row, col + 1], curr + 1)
+            }
+            used.delete(`${row},${col + 1}`)
+
+            //one column backwards
+            if (col - 1 >= 0 && !used.has(`${row},${col - 1}`) && board[row][col - 1] === word[curr]) {
+                found = found || backtrack([row, col - 1], curr + 1)
+            }
+            used.delete(`${row},${col + 1}`)
+
+            return found
+        }
+
         //cycle through board
         for (let i = 0; i < board.length; i++) {
             const row = board[i]
@@ -17,15 +51,13 @@ class Solution {
 
                 //check for valid path
                 if (backtrack([i, j], 1)) return true
+
+                //path failed clear the map
+                used.clear()
             }
             
         }
         //no valid path found
         return false
-    }
-
-    backtrack(pos) {
-        used.push(pos)
-        if (0) return
     }
 }
