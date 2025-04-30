0c88d3ee13e316761fe081e6a6dc2ff11604f8ca
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-04-30 19:37:22 -0400

    complete: solution

--- neetcode/Backtracking/Word_Search.js
+++ neetcode/Backtracking/Word_Search.js
@@ -10,33 +10,35 @@ class Solution {
         //backtrack function
         const backtrack = (pos, curr, found = false) => {
             const [row, col] = pos
-            used.set(`${row},${col}`, true)
+            used.set(`${row},${col}`, board[row][col])
+            curr += 1
+            if (curr >= word.length) return true
 
             //check the 4 directions, needs to be in bounds, not used, matches the next letter
 
             //one row down
             if (row + 1 < board.length && !used.has(`${row + 1},${col}`) && board[row+1][col] === word[curr]) {
-                found = found || backtrack([row+1, col], curr + 1)
+                found = found || backtrack([row+1, col], curr)
+                used.delete(`${row + 1},${col}`)
             }
-            used.delete(`${row + 1},${col}`)
 
             //one row up
             if (row - 1 >= 0 && !used.has(`${row - 1},${col}`) && board[row-1][col] === word[curr]) {
-                found = found || backtrack([row-1, col], curr + 1)
+                found = found || backtrack([row-1, col], curr)
+                used.delete(`${row - 1},${col}`)
             }
-            used.delete(`${row - 1},${col}`)
 
             //one column forward
             if (col + 1 < board[0].length && !used.has(`${row},${col + 1}`) && board[row][col + 1] === word[curr]) {
-                found = found || backtrack([row, col + 1], curr + 1)
+                found = found || backtrack([row, col + 1], curr)
+                used.delete(`${row},${col + 1}`)
             }
-            used.delete(`${row},${col + 1}`)
 
             //one column backwards
             if (col - 1 >= 0 && !used.has(`${row},${col - 1}`) && board[row][col - 1] === word[curr]) {
-                found = found || backtrack([row, col - 1], curr + 1)
+                found = found || backtrack([row, col - 1], curr)
+                used.delete(`${row},${col - 1}`)
             }
-            used.delete(`${row},${col + 1}`)
 
             return found
         }
@@ -50,7 +52,7 @@ class Solution {
                 if (letter !== word[0]) continue
 
                 //check for valid path
-                if (backtrack([i, j], 1)) return true
+                if (backtrack([i, j], 0)) return true
 
                 //path failed clear the map
                 used.clear()
