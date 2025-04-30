6ddb1bf859204e9ff184878254c074baaa72b270
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-04-30 14:44:21 -0400

    update: soltion in progress

--- neetcode/Backtracking/Word_Search.js
+++ neetcode/Backtracking/Word_Search.js
@@ -4,5 +4,28 @@ class Solution {
      * @param {string} word
      * @return {boolean}
      */
-    exist(board, word) {}
+    exist(board, word) {
+        const used = new Map()
+
+        //cycle through board
+        for (let i = 0; i < board.length; i++) {
+            const row = board[i]
+            for (let j = 0; j < row.length; j++) {
+                const letter = row[j]
+                //skip anything that is not the first letter
+                if (letter !== word[0]) continue
+
+                //check for valid path
+                if (backtrack([i, j], 1)) return true
+            }
+            
+        }
+        //no valid path found
+        return false
+    }
+
+    backtrack(pos) {
+        used.push(pos)
+        if (0) return
+    }
 }
