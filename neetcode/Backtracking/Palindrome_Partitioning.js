f80733e3fd0eef6b9c95e15eb983229c9e7ce228
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-05-06 14:10:20 -0400

    update: currently returns all palindromes with repeats

--- neetcode/Backtracking/Palindrome_Partitioning.js
+++ neetcode/Backtracking/Palindrome_Partitioning.js
@@ -3,5 +3,18 @@ class Solution {
      * @param {string} s
      * @return {string[][]}
      */
-    partition(s) {}
+    partition(s) {
+        const res = []
+        const backtrack = (string) => {
+            if (!string || string.length < 1) return
+            if (string === string.split('').reverse().join('')) res.push([string])
+            if (string.length === 1) return
+            for (let i = 1; i < string.length; i++) {
+                backtrack(string.substring(0,i))
+                backtrack(string.substring(i))
+            }
+        }
+        backtrack(s)
+        return res
+    }
 }
