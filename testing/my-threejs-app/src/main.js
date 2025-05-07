3f230060a40c2bf70137a176102687c289e84040
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-05-07 13:27:27 -0400

    add: basic renderer setup with delta time adjusted animation

--- testing/my-threejs-app/src/main.js
+++ testing/my-threejs-app/src/main.js
@@ -1,24 +1,43 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from 'three'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// create the scene, where objects get stored
+const scene = new THREE.Scene()
 
-setupCounter(document.querySelector('#counter'))
+// camera setup. 
+// perspective camera (field of view, aspect ratio, near clipping, far clipping)
+// clipping planes determine when an object no longer gets redered based on camera distance
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+camera.position.z = 2 // move the camera back 5 uinits from origin on z axis
+
+// renderer, displays everything will take a scene and camera later
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.getElementById('app').appendChild(renderer.domElement);
+
+// render a cube as example
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+//setup deltatime clocl
+let clock = new THREE.Clock()
+let lastTime = 0
+
+function animate() {
+  requestAnimationFrame(animate)
+
+  //get delta time so that animation are not dependant on framerate
+  const currentTime = clock.getElapsedTime()
+  const deltatime = currentTime - lastTime
+  lastTime = currentTime
+
+  const rotationSpeed = 1
+  cube.rotation.x += rotationSpeed * deltatime;
+  cube.rotation.y += rotationSpeed * deltatime;
+
+  renderer.render(scene, camera)
+}
+
+animate()
