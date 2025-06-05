d1ff32949382d0220bae9cd434bba7dcaf9aa31b
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-06-04 21:44:25 -0400

    update: complete solution

--- neetcode/heap/Contains_Duplicate.js
+++ neetcode/heap/Contains_Duplicate.js
@@ -3,5 +3,12 @@ class Solution {
      * @param {number[]} nums
      * @return {boolean}
      */
-    hasDuplicate(nums) {}
+    hasDuplicate(nums) {
+        const map = new Map()
+        for (let i = 0; i < nums.length; i++) {
+            if (map.has(nums[i])) return true
+            map.set(nums[i], 0)
+        }
+        return false
+    }
 }
