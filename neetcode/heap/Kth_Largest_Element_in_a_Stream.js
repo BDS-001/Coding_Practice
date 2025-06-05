98fa19aa7d16c24ad2c545996444ee50fa1e7a38
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-06-04 20:58:32 -0400

    update: completed solution

--- neetcode/heap/Kth_Largest_Element_in_a_Stream.js
+++ neetcode/heap/Kth_Largest_Element_in_a_Stream.js
@@ -3,11 +3,17 @@ class KthLargest {
      * @param {number} k
      * @param {number[]} nums
      */
-    constructor(k, nums) {}
+    constructor(k, nums) {
+        this.k = k
+        this.nums = nums
+    }
 
     /**
      * @param {number} val
      * @return {number}
      */
-    add(val) {}
+    add(val) {
+        this.nums.push(val)
+        return this.nums.sort((a, b) => b - a)[this.k - 1]
+    }
 }
