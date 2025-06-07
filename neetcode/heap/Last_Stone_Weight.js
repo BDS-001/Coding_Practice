a89b1d8d6d16846667134ac5d9b0765d32575863
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-06-07 12:48:16 -0400

    update: generate heap

--- neetcode/heap/Last_Stone_Weight.js
+++ neetcode/heap/Last_Stone_Weight.js
@@ -12,6 +12,15 @@ class Solution {
      * @return {number}
      */
     lastStoneWeight(stones) {
+        let heap = generateHeap(stones)
+    }
 
+    generateHeap(stones) {
+        if (stones.length < 1) return null
+        const head = new Node(stones[0])
+        stones.shift()
+        const center = Math.floor(stones.length / 2)
+        head.left = this.generateHeap(stones.splice(0, center))
+        head.right = this.generateHeap(stones[splice(center)])
     }
 }
