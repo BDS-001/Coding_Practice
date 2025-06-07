f648acaaedb62bbab4b72775ed8e642c9700618f
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-06-07 12:08:54 -0400

    update: add node class

--- neetcode/heap/Last_Stone_Weight.js
+++ neetcode/heap/Last_Stone_Weight.js
@@ -1,7 +1,17 @@
+class Node {
+    constructor(val = null, left = null, right = null) {
+        this.val = val
+        this.left = left
+        this.right = right
+    }
+}
+
 class Solution {
     /**
      * @param {number[]} stones
      * @return {number}
      */
-    lastStoneWeight(stones) {}
+    lastStoneWeight(stones) {
+
+    }
 }
