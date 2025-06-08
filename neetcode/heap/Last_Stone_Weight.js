304458d10fc07428d5d0f5d283c6a424f42cd81f
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-06-08 14:11:20 -0400

    update: pass starter tests, fails full test

--- neetcode/heap/Last_Stone_Weight.js
+++ neetcode/heap/Last_Stone_Weight.js
@@ -13,7 +13,9 @@ class Solution {
      */
     lastStoneWeight(stones) {
         stones.sort((a,b) => a - b)
-        let heap = generateMaxHeap(stones)
+        let heap = this.generateMaxHeap(stones)
+        const head = new Node(0, heap, null)
+        return this.simulate(head)
     }
 
     generateMaxHeap(stones) {
@@ -34,5 +36,44 @@ class Solution {
             q.push(right)
         }
         return head
+    }
+
+    smash(head) {
+        const stone1 = head
+        const stone2 = !head.right ? head.left : head.left.val > head.right.val ? head.left : head.right
+
+        stone1.val -= stone2.val
+        stone2.val -= stone1.val
+    }
+
+    organize(head, parent, direction) {
+        if (!head) return
+        if (head.val <= 0 && !head.left && !head.right) {
+            parent[direction] = null
+        }
+
+        if (head.left && head.val < head.left.val) {
+            const tmp = head.val
+            head.val = head.left.val
+            head.left.val = tmp
+        }
+
+        if (head.right && head.val < head.right.val) {
+            const tmp = head.val
+            head.val = head.right.val
+            head.right.val = tmp
+        }
+
+        if (head.left) this.organize(head.left, head, 'left')
+        if (head.right) this.organize(head.right, head, 'right')
+    }
+
+    simulate(head) {
+        const heap = head.left
+        while (heap && (heap.left || heap.right)) {
+            this.smash(heap)
+            this.organize(heap, head, 'left')
+        }
+        return head.left ? head.left.val : null
     }
 }
