12c72bb05d0abd258ee0456eaf106929277bd1d9
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-07-30 18:14:27 -0400

    update: soltion, passed 23/23

--- neetcode/Backtracking/Letter_Combinations_of_a_Phone_Number.js
+++ neetcode/Backtracking/Letter_Combinations_of_a_Phone_Number.js
@@ -1,7 +1,39 @@
 class Solution {
+    static phoneKeypad = {
+        '2': 'abc',
+        '3': 'def',
+        '4': 'ghi',
+        '5': 'jkl',
+        '6': 'mno',
+        '7': 'pqrs',
+        '8': 'tuv',
+        '9': 'wxyz'
+    };
+
+    constructor() {
+        this.res = []
+    }
+
     /**
      * @param {string} digits
      * @return {string[]}
      */
-    letterCombinations(digits) {}
+    letterCombinations(digits) {
+        if (!digits.length > 0) return this.res
+        this.backtrackRes(digits)
+        return this.res
+    }
+
+    backtrackRes(digits, curr = '') {
+        if (digits.length < 1) {
+            this.res.push(curr)
+            return
+        }
+
+        const letters = Solution.phoneKeypad[digits[0]]
+        for (let i = 0; i < letters.length; i++) {
+            this.backtrackRes(digits.slice(1), curr + letters[i])
+        }
+        return
+    }
 }
