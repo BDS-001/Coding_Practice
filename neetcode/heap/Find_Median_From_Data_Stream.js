11c945988b8fa91f88e5f1bad1dd0a595e322802
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-07-31 21:43:20 -0400

    update: current progress

--- neetcode/heap/Find_Median_From_Data_Stream.js
+++ neetcode/heap/Find_Median_From_Data_Stream.js
@@ -1,4 +1,9 @@
 class MedianFinder {
+    compare = {
+        'min': (a, b) => a < b,
+        'max': (a, b) => a > b
+    }
+
     constructor() {
         this.maxHeap = []
         this.minHeap = []
@@ -11,7 +16,12 @@ class MedianFinder {
      */
     addNum(num) {
         this.maxHeap.push(num)
-        this.heapifyUp(this.maxHeap, this.maxHeap.length - 1, (a, b) => a > b)
+        this.heapifyUp(this.maxHeap, this.maxHeap.length - 1, this.compare.max)
+
+        if(this.maxHeap.length - this.minHeap.length > 1) {
+            this.minHeap.push(this.maxHeap.shift())
+            this.heapifyUp(this.minHeap, this.minHeap.length - 1, this.compare.min)
+        }
 
     }
 
@@ -27,5 +37,22 @@ class MedianFinder {
             [heap[i], heap[parentIndex]] = [heap[parentIndex], heap[i]]
             i = parentIndex
         }
+    }
+
+    heapifyDown(heap, i, compare) {
+
+    }
+
+    dequeue(heap, heapType) {
+        if (!heap.length) return null
+        [heap[0], heap[heap.length - 1]] = [heap[heap.length - 1], heap[0]]
+        const value = heap.pop()
+        this.heapifyDown(heap, 0, this.compare[heapType])
+        return value
+    }
+
+    enqueue(heap, value, heapType) {
+        heap.push(value);
+        this.heapifyUp(heap, heap.length - 1, this.compare[heapType]);
     }
 }
