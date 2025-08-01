a6dae03d6a5a31e2a4898f09c55681e5ece64ea1
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-07-31 20:52:12 -0400

    update: current progress

--- neetcode/heap/Find_Median_From_Data_Stream.js
+++ neetcode/heap/Find_Median_From_Data_Stream.js
@@ -1,15 +1,31 @@
 class MedianFinder {
-    constructor() {}
+    constructor() {
+        this.maxHeap = []
+        this.minHeap = []
+    }
 
     /**
      *
      * @param {number} num
      * @return {void}
      */
-    addNum(num) {}
+    addNum(num) {
+        this.maxHeap.push(num)
+        this.heapifyUp(this.maxHeap, this.maxHeap.length - 1, (a, b) => a > b)
+
+    }
 
     /**
      * @return {number}
      */
     findMedian() {}
+
+    heapifyUp(heap, i, compare) {
+        while(i > 0) {
+            const parentIndex = Math.floor((i - 1) / 2);
+            if (!compare(heap[i], heap[parentIndex])) break
+            [heap[i], heap[parentIndex]] = [heap[parentIndex], heap[i]]
+            i = parentIndex
+        }
+    }
 }
