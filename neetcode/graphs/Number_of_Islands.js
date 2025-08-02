f9cc70643ded5e3b31d25febb054e4336b03a743
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-08-02 16:10:35 -0400

    update: current progress

--- neetcode/graphs/Number_of_Islands.js
+++ neetcode/graphs/Number_of_Islands.js
@@ -1,7 +1,32 @@
 class Solution {
+    
+    constructor() {
+        this.grid = []
+        this.used = []
+        this.islandCount = 0
+    }
     /**
      * @param {character[][]} grid
      * @return {number}
      */
-    numIslands(grid) {}
+    numIslands(grid) {
+        this.grid = grid
+
+        for (let row = 0; row < grid.length; row++) {
+            for (let col = 0; col < grid[row].length; col++) {
+                const tile = grid[row][col]
+                if (tile === 1 && !used.includes(`${row}-${col}`)) {
+                    exploreIsland()
+                    this.islandCount++;
+                }
+            }
+            
+        }
+
+        return this.islandCount
+    }
+
+    exploreIsland() {
+        // traverse grid for adjacent tiles, add each coordinate key to used
+    }
 }
