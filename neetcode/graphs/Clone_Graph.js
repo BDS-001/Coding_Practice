e64c0831eb84c834c99844a610d01ca144a2feca
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-09-05 21:52:47 -0400

    update: solution in progress

--- neetcode/graphs/Clone_Graph.js
+++ neetcode/graphs/Clone_Graph.js
@@ -9,9 +9,38 @@
  */
 
 class Solution {
+    constructor() {
+        this.map = new Map()
+    }
+
     /**
      * @param {Node} node
      * @return {Node}
      */
-    cloneGraph(node) {}
+    cloneGraph(node) {
+        this.mapNode(node)
+    }
+
+    mapNode(node) {
+        const queued = new Set()
+        const q = [node]
+
+
+        while(q.length > 0) {
+            const currentNode = q.shift()
+
+            const neighborsNums = currentNode.neighbors.reduce((acc, curr) => {
+                //add all non queued neighbors
+                if (!queued.has(curr.val)) {
+                    q.push(curr)
+                    queued.add(curr.val)
+                }
+                //return just the vals to the new array
+                acc.push(curr.val)
+                return acc
+            }, [])
+
+            this.map.set(currentNode.val, neighborsNums)
+        }
+    }
 }
