5dd82af2fe5d1693487305f6a3979a8cfa5830b0
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-09-07 21:17:42 -0400

    add: initial values function

--- neetcode/graphs/Rotting_Fruit.js
+++ neetcode/graphs/Rotting_Fruit.js
@@ -1,7 +1,24 @@
 class Solution {
+    constructor() {
+        this.fresh = new Set()
+        this.rotten = new Set()
+    }
+    
     /**
      * @param {number[][]} grid
      * @return {number}
      */
-    orangesRotting(grid) {}
+    orangesRotting(grid) {
+        this.getInitialValues(grid)
+    }
+
+    getInitialValues(grid) {
+        for (let row = 0; row < grid.length; row++) {
+            for (let col = 0; col < grid[row].length; col++) {
+                let cellValue = grid[row][col];
+                if (cellValue === 1) this.fresh.add(`${row}-${col}`)
+                if (cellValue === 2) this.rotten.add(`${row}-${col}`)
+            }
+        }
+    }
 }
