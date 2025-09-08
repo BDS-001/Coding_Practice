3db42b9a72f32766db49eca454b4490a4764aefb
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-09-07 21:51:50 -0400

    complete: pass 24/24

--- neetcode/graphs/Rotting_Fruit.js
+++ neetcode/graphs/Rotting_Fruit.js
@@ -10,6 +10,7 @@ class Solution {
      */
     orangesRotting(grid) {
         this.getInitialValues(grid)
+        return this.simulate()
     }
 
     getInitialValues(grid) {
@@ -20,5 +21,40 @@ class Solution {
                 if (cellValue === 2) this.rotten.add(`${row}-${col}`)
             }
         }
+    }
+
+    simulate() {
+        if (this.rotten.size === 0 && this.fresh.size > 0) return -1
+        let time = 0
+
+        while (this.fresh.size > 0) {
+            const freshSize = this.fresh.size
+            const currentRotten = [...this.rotten]
+            for (const val of currentRotten) {
+                const [row, col] = val.split('-').map(Number)
+                this.spread(row, col)
+            }
+            if (freshSize === this.fresh.size) return -1
+            time ++;
+        }
+        
+        return time
+    }
+
+    spread(row, col) {
+        const directions = [
+            [row-1, col],   // top
+            [row, col-1],   // left
+            [row, col+1],   // right
+            [row+1, col],   // bottom
+        ];
+
+        directions.forEach(([newRow, newCol]) => {
+            const coordinate = `${newRow}-${newCol}`;
+            if (this.fresh.has(coordinate)) {
+                this.fresh.delete(coordinate);
+                this.rotten.add(coordinate);
+            }
+        });
     }
 }
