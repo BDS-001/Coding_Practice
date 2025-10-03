e10ebc0ca65537630610d819433b2bc9902276bf
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-10-03 15:45:07 -0400

    add: blacklight support

--- testing/flashlight/script.js
+++ testing/flashlight/script.js
@@ -1,6 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
+    setupFlashlight()
+    setupBlacklight()
+})
+
+function setupFlashlight() {
     const flashlight = document.getElementById('flashlight')
     document.addEventListener('mousemove', (event) => {
         flashlight.style.transform = `translate(${event.clientX}px, ${event.clientY}px) translate(-50%, -50%)`
     })
-})
+}
+
+function setupBlacklight() {
+    const toggle = document.querySelector('.blacklightToggle')
+    const body = document.querySelector('body')
+    toggle.addEventListener('click', () => {
+        body.classList.toggle('blacklightActive')
+    })
+}
