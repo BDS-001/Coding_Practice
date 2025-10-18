b90187b94d71b2f969003ae2cd51ea2b44878049
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-10-17 23:44:59 -0400

    update: sultion in progress

--- neetcode/graphs/Course_Schedule.js
+++ neetcode/graphs/Course_Schedule.js
@@ -1,8 +1,30 @@
 class Solution {
+    constructor() {
+        this.map = new Map()
+    }
     /**
      * @param {number} numCourses
      * @param {number[][]} prerequisites
      * @return {boolean}
      */
-    canFinish(numCourses, prerequisites) {}
+    canFinish(numCourses, prerequisites) {
+        processPrereq(prerequisites)
+        return numCourses === countComplete()
+    }
+
+    processPrereq(prerequisites) {
+        for (let i = 0; i < prerequisites.length; i++) {
+            const [course, pre] = prerequisites[i]
+            if (!this.map.has(course)) this.map.set(course, new Set())
+            if (!this.map.has(pre)) this.map.set(pre, new Set())
+            this.map.get(course).push(pre)
+        }
+    }
+
+    countComplete() {
+        
+    }
 }
+
+const test = new Solution()
+test.canFinish(2, [[0,1]])
