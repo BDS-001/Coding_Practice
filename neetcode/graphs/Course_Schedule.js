456f687dab38e91c4f633caa4919d9e6291b91a0
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-10-18 00:14:09 -0400

    update: solution in progress, passes 8/26

--- neetcode/graphs/Course_Schedule.js
+++ neetcode/graphs/Course_Schedule.js
@@ -8,8 +8,9 @@ class Solution {
      * @return {boolean}
      */
     canFinish(numCourses, prerequisites) {
-        processPrereq(prerequisites)
-        return numCourses === countComplete()
+        if (!prerequisites || prerequisites.length === 0) return true
+        this.processPrereq(prerequisites)
+        return numCourses === this.countComplete()
     }
 
     processPrereq(prerequisites) {
@@ -17,14 +18,31 @@ class Solution {
             const [course, pre] = prerequisites[i]
             if (!this.map.has(course)) this.map.set(course, new Set())
             if (!this.map.has(pre)) this.map.set(pre, new Set())
-            this.map.get(course).push(pre)
+            this.map.get(course).add(pre)
         }
     }
 
     countComplete() {
-        
+        let complete = 0
+        this.map.forEach((val, key) => {
+            if (this.canComplete(key)) complete++
+        })
+        return complete
+    }
+
+    canComplete(key, visited = new Set()) {
+        if (visited.has(key)) return false
+        visited.add(key)
+
+        const pre = this.map.get(key)
+
+        for (const val of pre) {
+            if (!this.canComplete(val, visited)) return false
+        }
+        return true
     }
 }
 
 const test = new Solution()
-test.canFinish(2, [[0,1]])
+//fails should be true
+test.canFinish(5,[[1,4],[2,4],[3,1],[3,2]])
