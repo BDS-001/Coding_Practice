230bae1c262d29576fb666f8206fe93785c6b2ff
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-10-19 15:17:21 -0400

    add: course map setup

--- neetcode/graphs/Course_Schedule_II.js
+++ neetcode/graphs/Course_Schedule_II.js
@@ -1,8 +1,28 @@
 class Solution {
+    
+    constructor() {
+        this.map = []
+    }
+
     /**
      * @param {number} numCourses
      * @param {number[][]} prerequisites
      * @return {number[]}
      */
-    findOrder(numCourses, prerequisites) {}
+    findOrder(numCourses, prerequisites) {
+        if (!prerequisites || prerequisites.length === 0) return true
+        this.setupMap(numCourses)
+        this.processPrereq(prerequisites)
+    }
+
+    processPrereq(prerequisites) {
+        for (let i = 0; i < prerequisites.length; i++) {
+            const [course, pre] = prerequisites[i]
+            this.map[course].add(pre)
+        }
+    }
+
+    setupMap(n) {
+        this.map = Array.from({length: n}, (_, i) => new Set())
+    }
 }
