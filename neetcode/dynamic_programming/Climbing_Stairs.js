e331192755acdee4d057cb40509c24f801e95dee
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-11-11 21:28:04 -0500

    update: code cleanup

--- neetcode/dynamic_programming/Climbing_Stairs.js
+++ neetcode/dynamic_programming/Climbing_Stairs.js
@@ -1,26 +1,16 @@
 class Solution {
-    constructor() {
-        this.total = 0
-        this.n
-    }
-
     /**
      * @param {number} n
      * @return {number}
      */
     climbStairs(n) {
-        this.n = n
-        this.climb()
-        return this.total
+        return this.climb(0, n);
     }
 
-    climb(curr=0) {
-        if (this.n < curr) return
-        if (this.n === curr) {
-            this.total++
-            return
-        }
-        this.climb(curr + 1)
-        this.climb(curr + 2)
+    climb(curr, target) {
+        if (curr > target) return 0;
+        if (curr === target) return 1;
+
+        return this.climb(curr + 1, target) + this.climb(curr + 2, target);
     }
 }
