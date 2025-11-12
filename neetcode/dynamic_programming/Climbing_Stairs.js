d5b8429f8856ee532c3d31d496056f2e5fcb335a
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-11-11 21:25:25 -0500

    solution complete: recursive

--- neetcode/dynamic_programming/Climbing_Stairs.js
+++ neetcode/dynamic_programming/Climbing_Stairs.js
@@ -1,7 +1,26 @@
 class Solution {
+    constructor() {
+        this.total = 0
+        this.n
+    }
+
     /**
      * @param {number} n
      * @return {number}
      */
-    climbStairs(n) {}
+    climbStairs(n) {
+        this.n = n
+        this.climb()
+        return this.total
+    }
+
+    climb(curr=0) {
+        if (this.n < curr) return
+        if (this.n === curr) {
+            this.total++
+            return
+        }
+        this.climb(curr + 1)
+        this.climb(curr + 2)
+    }
 }
