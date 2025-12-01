cf66cf667519fb2399cdc480bd936139d51f2851
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-12-01 18:54:47 -0500

    complete: solution pass 30/30

--- neetcode/dynamic_programming/Min_Cost_Climbing_Stairs.js
+++ neetcode/dynamic_programming/Min_Cost_Climbing_Stairs.js
@@ -3,5 +3,14 @@ class Solution {
      * @param {number[]} cost
      * @return {number}
      */
-    minCostClimbingStairs(cost) {}
+    minCostClimbingStairs(cost) {
+        const start1 = this.calculatePath(cost, 0)
+        const start2 = this.calculatePath(cost, 1)
+        return Math.min(start1, start2)
+    }
+
+    calculatePath(cost, i) {
+        if (i >= cost.length) return 0
+        return cost[i] + Math.min(this.calculatePath(cost, i+1), this.calculatePath(cost, i+2))
+    }
 }
