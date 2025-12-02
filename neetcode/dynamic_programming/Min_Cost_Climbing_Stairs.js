3c4778fe0e5ec15f2de00f614cb625bf79808676
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-12-01 20:02:03 -0500

    update: now using DP memoization solution

--- neetcode/dynamic_programming/Min_Cost_Climbing_Stairs.js
+++ neetcode/dynamic_programming/Min_Cost_Climbing_Stairs.js
@@ -1,16 +1,25 @@
 class Solution {
+    constructor() {
+        this.cost
+        this.cache = new Map()
+    }
     /**
      * @param {number[]} cost
      * @return {number}
      */
     minCostClimbingStairs(cost) {
-        const start1 = this.calculatePath(cost, 0)
-        const start2 = this.calculatePath(cost, 1)
+        this.cost = cost
+        const start1 = this.calculatePath(0)
+        const start2 = this.calculatePath(1)
         return Math.min(start1, start2)
     }
 
-    calculatePath(cost, i) {
-        if (i >= cost.length) return 0
-        return cost[i] + Math.min(this.calculatePath(cost, i+1), this.calculatePath(cost, i+2))
+    calculatePath(i) {
+        if (i >= this.cost.length) return 0
+        if (this.cache.has(i)) return this.cache.get(i)
+        
+        const minCost = this.cost[i] + Math.min(this.calculatePath(i+1), this.calculatePath(i+2))
+        this.cache.set(i, minCost)
+        return minCost
     }
 }
