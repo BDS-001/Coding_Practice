3e995fa6177dae546862a825b8f34fe31eeca3d7
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-12-02 20:22:57 -0500

    update: solution pass 24/30

--- neetcode/dynamic_programming/House_Robber_II.js
+++ neetcode/dynamic_programming/House_Robber_II.js
@@ -1,7 +1,31 @@
 class Solution {
+    constructor () {
+        this.cache = new Map()
+        this.houses
+    }
     /**
      * @param {number[]} nums
      * @return {number}
      */
-    rob(nums) {}
+    rob(nums) {
+        if (nums.length <= 3) return Math.max(...nums)
+        this.houses = nums
+        const startFirstHouse = this.getMaxVal(0, true)
+        const startSecondHouse = this.getMaxVal(1)
+        const startThirdHouse = this.getMaxVal(2)
+        return Math.max(startFirstHouse, startSecondHouse, startThirdHouse)
+    }
+
+    getMaxVal(i, start=false) {
+        if (i >= this.houses.length) return 0
+        if (start && i === this.houses.length-1) return 0
+        if (this.cache.has(i)) return this.cache.get(i)
+
+        const firstAvailable = this.getMaxVal(i+2, start)
+        const secondAvailableNeighbour = this.getMaxVal(i+3, start)
+        const maxAmount = this.houses[i] + Math.max(firstAvailable, secondAvailableNeighbour)
+        
+        this.cache.set(i, maxAmount)
+        return maxAmount
+    }
 }
