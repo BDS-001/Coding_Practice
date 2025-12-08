b4ae1696bfbe87b544c73855daa1014bd5effd18
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-12-07 20:04:34 -0500

    update: current code fails, it searches for sequence not substings

--- neetcode/dynamic_programming/Longest_Palindromic_Substring.js
+++ neetcode/dynamic_programming/Longest_Palindromic_Substring.js
@@ -1,7 +1,30 @@
 class Solution {
+
+    constructor() {
+        this.s = ''
+        this.max = ''
+    }
     /**
      * @param {string} s
      * @return {string}
      */
-    longestPalindrome(s) {}
+    longestPalindrome(s) {
+        this.s = s
+        this.search()
+        return this.max
+    }
+
+    search(i = 0, substring = '') {
+        if (i >= this.s.length) return
+        const newString = substring + this.s[i]
+        if (this.checkPalindrome(newString) && this.max.length < newString.length) this.max = newString
+        this.search(i + 1, substring)
+        this.search(i + 1, newString)
+    }
+
+    checkPalindrome(substring) {
+        return substring === [...substring].reverse().join('')
+    }
 }
+
+console.log(new Solution().longestPalindrome("aacabdkacaa"))
