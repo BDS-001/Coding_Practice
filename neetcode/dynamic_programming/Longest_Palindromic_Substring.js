76cb166f03b93d5625467623786e27f78badfa0e
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-12-08 22:28:00 -0500

    update: soltion pass 27/27

--- neetcode/dynamic_programming/Longest_Palindromic_Substring.js
+++ neetcode/dynamic_programming/Longest_Palindromic_Substring.js
@@ -9,22 +9,28 @@ class Solution {
      * @return {string}
      */
     longestPalindrome(s) {
+        if (this.checkPalindrome(s)) return s
         this.s = s
         this.search()
         return this.max
     }
 
-    search(i = 0, substring = '') {
-        if (i >= this.s.length) return
-        const newString = substring + this.s[i]
-        if (this.checkPalindrome(newString) && this.max.length < newString.length) this.max = newString
-        this.search(i + 1, substring)
-        this.search(i + 1, newString)
+    search() {
+        for (let start = 0; start < this.s.length; start++) {
+            let end = this.s.length
+
+            while(end > start && end - start > this.max.length) {
+                const sub = this.s.slice(start, end)
+                if (this.checkPalindrome(sub)) {
+                    if(this.max.length < sub.length) this.max = sub
+                    break 
+                }
+                end--
+            }
+        }
     }
 
     checkPalindrome(substring) {
         return substring === [...substring].reverse().join('')
     }
 }
-
-console.log(new Solution().longestPalindrome("aacabdkacaa"))
