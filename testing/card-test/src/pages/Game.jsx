1de954ca1ee23110adb49be99da24988c8d1634a
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-12-12 18:12:29 -0500

    add: random peak tilt

--- testing/card-test/src/pages/Game.jsx
+++ testing/card-test/src/pages/Game.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react';
 
 export default function Game() {
-    const [tilts] = useState(() => [...Array(5)].map(() => Math.random() * 20 - 10));
+    const [tilts] = useState(() => [...Array(20)].map(() => {
+        const tilt = Math.floor(Math.random() * 20 - 10)
+        const peak = tilt > 0 ? 40 : -40
+        return {tilt, peak}
+    }));
 
     return (
         <div className="gameGrid">
-            {tilts.map((tilt, key) => (
-                <div className="gameCard" key={key} style={{ '--tilt': `${tilt}deg` }}></div>
+            {tilts.map((card, key) => (
+                <div className="gameCard" key={key} style={{ '--tilt': `${card.tilt}deg`, '--peak': `${card.peak}deg` }}></div>
             ))}
         </div>
     )
