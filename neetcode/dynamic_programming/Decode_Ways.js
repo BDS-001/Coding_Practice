cb8acd69a5f80d102c60c8a5f7d89d0089b646a5
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-12-18 22:25:10 -0500

    update: solution completes 15/25 tests

--- neetcode/dynamic_programming/Decode_Ways.js
+++ neetcode/dynamic_programming/Decode_Ways.js
@@ -4,20 +4,27 @@ class Solution {
      * @return {number}
      */
     numDecodings(s) {
-        if (!s || s[0] === '0' || s.includes('00') || !this.validBase(s)) return 0
+        if (!s || s[0] === '0' || s.includes('00')) return 0
+
+        const paired = new Set()
+
+        for (let i = 0; i < s.length; i++) {
+            if (s[i] === '0') {
+                if (i === 0 || s[i-1] === '0' || Number(s[i-1] + s[i]) > 26) return 0
+                paired.add(i-1)
+                paired.add(i)
+            }
+        }
+
         let count = 1
         for (let i = 0; i < s.length - 1; i++) {
-            if (s[i+1] === '0' || s[i] === '0') continue
+            if (s[i] === '0' || s[i+1] === '0') continue
+            if (paired.has(i) || paired.has(i+1)) continue
             if (Number(s[i] + s[i+1]) <= 26) count++
         }
+
         return count
     }
-
-    validBase(s) {
-        for (let i = 0; i < s.length; i++) {
-            if (s[i] !== '0') continue
-            if (Number(s[i-1] + s[i]) > 26) return false
-        }
-        return true
-    }
 }
+
+//fail s="1123"
