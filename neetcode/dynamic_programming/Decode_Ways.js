5663d81a6c49337586949192140ec61c0b414e50
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-12-18 21:52:48 -0500

    update: current solution passes 13/25

--- neetcode/dynamic_programming/Decode_Ways.js
+++ neetcode/dynamic_programming/Decode_Ways.js
@@ -4,6 +4,20 @@ class Solution {
      * @return {number}
      */
     numDecodings(s) {
-        
+        if (!s || s[0] === '0' || s.includes('00') || !this.validBase(s)) return 0
+        let count = 1
+        for (let i = 0; i < s.length - 1; i++) {
+            if (s[i+1] === '0' || s[i] === '0') continue
+            if (Number(s[i] + s[i+1]) <= 26) count++
+        }
+        return count
+    }
+
+    validBase(s) {
+        for (let i = 0; i < s.length; i++) {
+            if (s[i] !== '0') continue
+            if (Number(s[i-1] + s[i]) > 26) return false
+        }
+        return true
     }
 }
