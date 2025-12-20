3cda530bdb0c9e6a2b0b36dddbfb0c516058ebf4
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-12-19 19:59:11 -0500

    update: new solution in progress

--- neetcode/dynamic_programming/Decode_Ways.js
+++ neetcode/dynamic_programming/Decode_Ways.js
@@ -1,29 +1,34 @@
 class Solution {
+    constructor() {
+        this.strArr = []
+    }
     /**
      * @param {string} s
      * @return {number}
      */
     numDecodings(s) {
-        if (!s || s[0] === '0' || s.includes('00')) return 0
+        if (!this.valid(s)) return 0
+        this.process(s)
+    }
 
-        const paired = new Set()
+    valid(s) {
+        if (s[0] === '0' || s.includes('00')) return false
 
         for (let i = 0; i < s.length; i++) {
-            if (s[i] === '0') {
-                if (i === 0 || s[i-1] === '0' || Number(s[i-1] + s[i]) > 26) return 0
-                paired.add(i-1)
-                paired.add(i)
-            }
+            if (s[i] !== '0') continue
+            if (Number(s[i-1] + s[i]) > 26) return false
         }
 
-        let count = 1
-        for (let i = 0; i < s.length - 1; i++) {
-            if (s[i] === '0' || s[i+1] === '0') continue
-            if (paired.has(i) || paired.has(i+1)) continue
-            if (Number(s[i] + s[i+1]) <= 26) count++
-        }
+        return true
+    }
 
-        return count
+    process(s) {
+        let arr = s.split('')
+        while (arr.length > 0) {
+            const last = arr.pop()
+            const num = last === '0' ? arr.pop() + last : last
+            this.strArr.unshift(num)
+        }
     }
 }
 
