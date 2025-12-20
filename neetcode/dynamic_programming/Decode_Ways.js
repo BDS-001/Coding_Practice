8bd8ddee3069e610c8d184fd8b36db3e965592d0
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-12-19 20:00:55 -0500

    update: combine process and valid to use one loop

--- neetcode/dynamic_programming/Decode_Ways.js
+++ neetcode/dynamic_programming/Decode_Ways.js
@@ -7,28 +7,25 @@ class Solution {
      * @return {number}
      */
     numDecodings(s) {
-        if (!this.valid(s)) return 0
-        this.process(s)
-    }
-
-    valid(s) {
-        if (s[0] === '0' || s.includes('00')) return false
-
-        for (let i = 0; i < s.length; i++) {
-            if (s[i] !== '0') continue
-            if (Number(s[i-1] + s[i]) > 26) return false
-        }
-
-        return true
+        if (!this.process(s)) return 0
     }
 
     process(s) {
+        if (s[0] === '0' || s.includes('00')) return false
+
         let arr = s.split('')
         while (arr.length > 0) {
             const last = arr.pop()
-            const num = last === '0' ? arr.pop() + last : last
-            this.strArr.unshift(num)
+            if (last === '0') {
+                const prev = arr.pop()
+                if (Number(prev + last) > 26) return false
+                this.strArr.unshift(prev + last)
+            } else {
+                this.strArr.unshift(last)
+            }
         }
+
+        return true
     }
 }
 
