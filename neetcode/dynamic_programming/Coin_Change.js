6e30d863edf76dda71a2afcb24fc5fce8235873f
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-12-22 21:07:31 -0500

    update: sort coins largest to smallest

--- neetcode/dynamic_programming/Coin_Change.js
+++ neetcode/dynamic_programming/Coin_Change.js
@@ -5,6 +5,10 @@ class Solution {
      * @return {number}
      */
     coinChange(coins, amount) {
+        if (!coins || coins.length === 0) return -1
+        coins = coins.sort((a, b) => b-a)
+        console.log(coins)
+        if (coins[0] > amount) return -1
         
     }
 }
