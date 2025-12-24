cac50fbd84fe3a2ace04020068f568b4e84bf4e5
BDS-001 <113209967+BDS-001@users.noreply.github.com>
2025-12-23 23:14:08 -0500

    update: cleanup solution code

--- neetcode/dynamic_programming/Coin_Change.js
+++ neetcode/dynamic_programming/Coin_Change.js
@@ -1,37 +1,28 @@
 class Solution {
-    constructor() {
-        this.coins = []
-        this.cache = new Map()
-    }
     /**
      * @param {number[]} coins
      * @param {number} amount
      * @return {number}
      */
     coinChange(coins, amount) {
-        if (amount === 0) return 0
-        if (!coins || coins.length === 0) return -1
-        this.coins = coins.sort((a, b) => b-a)
-        if (this.coins[coins.length - 1] > amount) return -1
-        const small = this.check(amount)
-        return small === Infinity ? -1 : small
+        this.coins = coins
+        this.cache = new Map()
+
+        const result = this.check(amount)
+        return result === Infinity ? -1 : result
     }
 
-    check(remainder, total=0) {
-        if (remainder < 0) return Infinity
+    check(remainder) {
         if (remainder === 0) return 0
-
-        let small = Infinity
+        if (remainder < 0) return Infinity
         if (this.cache.has(remainder)) return this.cache.get(remainder)
-        for (let i = 0; i < this.coins.length; i++) {
-            const val = this.coins[i]
-            if (val > remainder) continue 
-            small = Math.min(small, 1 + this.check(remainder - val, total+this.coins[i]))
+
+        let min = Infinity
+        for (const coin of this.coins) {
+            min = Math.min(min, 1 + this.check(remainder - coin))
         }
-        this.cache.set(remainder, small)
-        return small
+
+        this.cache.set(remainder, min)
+        return min
     }
 }
-
-coins=[357,239,73,52]
-amount=9832
