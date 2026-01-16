7611c894b408077d396aba0d62419f7e0200594c
BDS <113209967+BDS-001@users.noreply.github.com>
2026-01-15 20:42:05 -0500

    update: bruteforce

--- neetcode/dynamic_programming/Maximum_Product_Subarray.js
+++ neetcode/dynamic_programming/Maximum_Product_Subarray.js
@@ -3,5 +3,16 @@ class Solution {
      * @param {number[]} nums
      * @return {number}
      */
-    maxProduct(nums) {}
+    maxProduct(nums) {
+        let max = -Infinity
+        for (let start = 0; start < nums.length; start++) {
+            for (let end = start+1; end <= nums.length; end++) {
+                const val = nums.slice(start, end).reduce((acc, num) => acc * num, 1)
+                max = Math.max(val, max)
+            }
+        }    
+        return max    
+    }
+
+    
 }
