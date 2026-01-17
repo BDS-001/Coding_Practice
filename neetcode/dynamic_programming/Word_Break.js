82229d95af460ce5d87404a06923f2fdfe77c7cf
BDS <113209967+BDS-001@users.noreply.github.com>
2026-01-17 14:21:18 -0500

    update: brute force solution

--- neetcode/dynamic_programming/Word_Break.js
+++ neetcode/dynamic_programming/Word_Break.js
@@ -4,5 +4,17 @@ class Solution {
      * @param {string[]} wordDict
      * @return {boolean}
      */
-    wordBreak(s, wordDict) {}
+    wordBreak(s, wordDict) {
+        const q = [s]
+        while (q.length > 0) {
+            const currentString = q.pop()
+            for (const word of wordDict) {
+                const sub = currentString.slice(0, word.length)
+                if (sub !== word) continue
+                const remaining = currentString.slice(word.length)
+                if (remaining === '') return true
+                q.push(remaining)
+            }
+        }
+    }
 }
