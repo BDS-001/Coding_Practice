48514dad54bf5d30339c2b97b461c7b809be2461
BDS <113209967+BDS-001@users.noreply.github.com>
2026-01-18 21:43:00 -0500

    update: solution in progress

--- neetcode/dynamic_programming/Longest_Increasing_Subsequence.js
+++ neetcode/dynamic_programming/Longest_Increasing_Subsequence.js
@@ -1,7 +1,26 @@
 class Solution {
+    constructor() {
+        this.map = new Map()
+        this.globalMax = -Infinity
+    }
     /**
      * @param {number[]} nums
      * @return {number}
      */
-    lengthOfLIS(nums) {}
+    lengthOfLIS(nums, index=0) {
+
+    }
+
+    DP(nums, index=0) {
+        if (index === nums.length-1) return 1
+        if (this.map.has(index)) return this.map.get(index)
+        let max = 1
+        for (let i = index+1; i < nums.length; i++) {
+            if (nums[index] > nums[i]) continue
+            max = Math.max(max, 1 + this.DP(nums, i))
+        }
+        this.globalMax = Math.max(max, this.globalMax)
+        this.map.set(index, max)
+        return max
+    }
 }
