5fc6520d562371e14fde6004e84d23097ad7007a
BDS <113209967+BDS-001@users.noreply.github.com>
2026-01-20 14:40:29 -0500

    update: solution complete

--- neetcode/Intervals/Insert_Interval.js
+++ neetcode/Intervals/Insert_Interval.js
@@ -5,9 +5,26 @@ class Solution {
      * @return {number[][]}
      */
     insert(intervals, newInterval) {
+        const replace = []
+        let insertIdx = intervals.length
+
         for (let i = 0; i < intervals.length; i++) {
             if (newInterval[0] > intervals[i][1]) continue
-            if (newInterval[0] > newInterval[0] && newInterval[1] < intervals[i][1]) return intervals
+            if (newInterval[1] < intervals[i][0]) {
+                insertIdx = i
+                break
+            }
+            replace.push(i)
         }
+
+        if (replace.length === 0) {
+            return [...intervals.slice(0, insertIdx), newInterval, ...intervals.slice(insertIdx)]
+        }
+
+        const newInt = [
+            Math.min(newInterval[0], intervals[replace[0]][0]),
+            Math.max(newInterval[1], intervals[replace[replace.length - 1]][1])
+        ]
+        return [...intervals.slice(0, replace[0]), newInt, ...intervals.slice(replace[replace.length - 1] + 1)]
     }
 }
