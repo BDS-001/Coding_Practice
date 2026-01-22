efab395d38682fa35c97f3c6763e4b8f181712b0
BDS <113209967+BDS-001@users.noreply.github.com>
2026-01-21 21:24:34 -0500

    current solution: pass 8/21

--- neetcode/Intervals/Merge_Intervals.js
+++ neetcode/Intervals/Merge_Intervals.js
@@ -3,5 +3,22 @@ class Solution {
      * @param {number[][]} intervals
      * @return {number[][]}
      */
-    merge(intervals) {}
+    merge(intervals) {
+        const newIntervals = []
+        let newRange = intervals[0]
+
+        for (let index = 1; index < intervals.length; index++) {
+            const range = intervals[index];
+
+            if (range[0] > newRange[1]) {
+                newIntervals.push(newRange)
+                newRange = range
+                continue
+            }
+            
+            newRange = [newRange[0], Math.max(newRange[1], range[1])]
+        }
+        newIntervals.push(newRange)
+        return newIntervals
+    }
 }
