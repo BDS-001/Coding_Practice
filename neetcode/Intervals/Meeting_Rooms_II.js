1ceaf08fb2f890656ab0ec3e62978b1a28470f04
BDS <113209967+BDS-001@users.noreply.github.com>
2026-02-12 17:06:42 -0500

    update: solution in progress

--- neetcode/Intervals/Meeting_Rooms_II.js
+++ neetcode/Intervals/Meeting_Rooms_II.js
@@ -13,5 +13,15 @@ class Solution {
      * @param {Interval[]} intervals
      * @returns {number}
      */
-    minMeetingRooms(intervals) {}
+    minMeetingRooms(intervals) {
+        intervals.sort((a, b) => a.start - b.start)
+        const remaining = []
+        let days = 1
+
+        let prev = intervals[0].end
+        for (let i = 1; i < array.length; i++) {
+            if (intervals[i].start >= intervals[i-1].end) continue
+            intervals[i].end < intervals[i-1].end ? remaining.push(intervals[i-1]) : remaining.push(intervals[i])
+        }
+    }
 }
