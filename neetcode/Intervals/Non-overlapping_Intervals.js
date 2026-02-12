38039f93e1f84e7722d960725ea84140666dcf7b
BDS <113209967+BDS-001@users.noreply.github.com>
2026-02-12 13:11:51 -0500

    update: brute force solution pass 16/23, time limit exceeded

--- neetcode/Intervals/Non-overlapping_Intervals.js
+++ neetcode/Intervals/Non-overlapping_Intervals.js
@@ -3,5 +3,27 @@ class Solution {
      * @param {number[][]} intervals
      * @return {number}
      */
-    eraseOverlapIntervals(intervals) {}
+    eraseOverlapIntervals(intervals) {
+        intervals.sort((a, b) => a[0] - b[0])
+        return intervals.length - this.bruteForce(intervals)
+    }
+
+    bruteForce(intervals) {
+        if (intervals.length === 1) return 1
+
+        let overlap = false
+        for (let i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] < intervals[i-1][1]) {
+                overlap = true
+                break
+            }
+        }
+        if (overlap === false) return intervals.length
+
+        let leastRemoved = -1
+        for (let i = 0; i < intervals.length; i++) {
+            leastRemoved = Math.max(leastRemoved, this.bruteForce([...intervals.slice(0, i), ...intervals.slice(i+1)]))
+        }
+        return leastRemoved
+    }
 }
