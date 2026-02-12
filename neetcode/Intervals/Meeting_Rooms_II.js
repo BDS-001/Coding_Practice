6aed7b6c03a14b25e60c4de325aa08a88146ce3c
BDS <113209967+BDS-001@users.noreply.github.com>
2026-02-12 15:51:32 -0500

    initial commit

---
+++ neetcode/Intervals/Meeting_Rooms_II.js
@@ -0,0 +1,17 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ *   constructor(start, end) {
+ *     this.start = start;
+ *     this.end = end;
+ *   }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {Interval[]} intervals
+     * @returns {number}
+     */
+    minMeetingRooms(intervals) {}
+}
