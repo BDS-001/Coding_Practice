ddbe8e27a27ebd4d8906cdac24dbc9c9b41b9cb8
BDS <113209967+BDS-001@users.noreply.github.com>
2026-02-18 23:56:56 -0500

    solution complete, pass 21/21

--- neetcode/Intervals/Minimum_Interval_to_Include_Each_Query.js
+++ neetcode/Intervals/Minimum_Interval_to_Include_Each_Query.js
@@ -5,15 +5,21 @@ class Solution {
      * @return {number[]}
      */
     minInterval(intervals, queries) {
-        const size = ([s, e]) => e - s;
+        const size = ([s, e]) => e - s + 1;
         intervals.sort((a, b) => size(a) - size(b));
         const res = []
         queries.forEach((q) => {
+            let ans = -1
             for (let i = 0; i < intervals.length; i++) {
                 const interval = intervals[i];
-                
+                if (q >= interval[0] && q<= interval[1]) {
+                    ans = size(interval)
+                    break
+                }
             }
+            res.push(ans)
         })
+        return res
     }
 }
 
