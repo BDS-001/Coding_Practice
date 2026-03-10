7f9d88c19a11f5796d4b2dd811a001e5aab44be7
BDS <113209967+BDS-001@users.noreply.github.com>
2026-03-09 22:10:42 -0400

    complete: DP solution pass 30/30

--- neetcode/greedy/JumpGame.js
+++ neetcode/greedy/JumpGame.js
@@ -1,7 +1,26 @@
 class Solution {
+
+    constructor() {
+        this.cache = new Map()
+        this.nums = []
+    }
     /**
      * @param {number[]} nums
      * @return {boolean}
      */
-    canJump(nums) {}
+    canJump(nums) {
+        this.nums = nums
+        return this.dp(0) 
+    }
+
+    dp(index) {
+        if (index >= this.nums.length -1) return true
+        if (this.cache.has(index)) return this.cache.get(index)
+        let valid = false
+        for (let i = 1; i <= this.nums[index] ; i++) {
+            valid = valid || this.dp(index+i)
+        }
+        this.cache.set(index, valid)
+        return valid
+    }
 }
