39b076826bbb47add780a03645d64678c6ef4b55
BDS <113209967+BDS-001@users.noreply.github.com>
2026-03-10 21:08:50 -0400

    initial commit

---
+++ neetcode/greedy/JumpGameII.js
@@ -0,0 +1,11 @@
+class Solution {
+    constructor() {
+        this.cache = new Map()
+        this.nums = []
+    }
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    jump(nums) {}
+}
