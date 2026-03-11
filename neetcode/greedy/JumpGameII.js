28a2901127d6a421ead85b1e4133fe0a4cf1a47c
BDS <113209967+BDS-001@users.noreply.github.com>
2026-03-10 21:23:40 -0400

    complete: dp solution pass 23/23

--- neetcode/greedy/JumpGameII.js
+++ neetcode/greedy/JumpGameII.js
@@ -7,5 +7,21 @@ class Solution {
      * @param {number[]} nums
      * @return {number}
      */
-    jump(nums) {}
+    jump(nums) {
+        this.nums = nums
+        return this.dp(0)
+    }
+
+    dp(i) {
+        if (this.cache.has(i)) return this.cache.get(i)
+        if (i >= this.nums.length-1) return 0
+        let min = Infinity
+        if (this.nums[i] == 0) return min
+
+        for (let range = this.nums[i]; range > 0; range--) {
+            min = Math.min(min, 1 + this.dp(i+range))
+        }
+        this.cache.set(i, min)
+        return min
+    }
 }
